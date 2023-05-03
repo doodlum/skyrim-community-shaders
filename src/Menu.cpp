f4ce09eb47421e2a7e148adbed37039ce5ac530a
ffarrell17 <116548954+ffarrell17@users.noreply.github.com>
2023-05-03 20:45:34 +0100

    Converted UI from reshade to native imgui

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -19,6 +19,13 @@ void SetupImGuiStyle()
 }
 bool IsEnabled = false;
 
+Menu::~Menu()
+{
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+}
+
 void Menu::Load(json& o_json)
 {
 	if (o_json[SETTING_MENU_TOGGLEKEY].is_number_unsigned()) {
@@ -36,7 +43,7 @@ void Menu::Save(json& o_json)
 
 RE::BSEventNotifyControl Menu::ProcessEvent(RE::InputEvent* const* a_event, RE::BSTEventSource<RE::InputEvent*>* a_eventSource)
 {
-	if (!a_event || !a_eventSource)
+	 if (!a_event || !a_eventSource)
 		return RE::BSEventNotifyControl::kContinue;
 
 	for (auto event = *a_event; event; event = event->next) {
@@ -132,20 +139,38 @@ RE::BSEventNotifyControl Menu::ProcessEvent(RE::InputEvent* const* a_event, RE::
 				break;
 			}
 
-			switch (button->device.get()) {
+			auto& io = ImGui::GetIO();
+			switch (button->device.get()) 
+			{
 			case RE::INPUT_DEVICE::kKeyboard:
-				if (key == toggleKey && !button->IsPressed()) {
-
-					// Avoid closing menu when setting the toggle key
-					if (settingToggleKey) {
+				if (!button->IsPressed()) 
+				{
+					if (settingToggleKey) 
+					{
+						toggleKey = key;
 						settingToggleKey = false;
-						break;
+					} 
+					else if (key == toggleKey) 
+					{
+						IsEnabled = !IsEnabled;
+						if (const auto controlMap = RE::ControlMap::GetSingleton()) 
+						{
+							controlMap->ignoreKeyboardMouse = IsEnabled;
+						}
+					} 
+					else 
+					{
+						io.AddKeyEvent(VirtualKeyToImGuiKey(key), button->IsPressed());
 					}
-
-					IsEnabled = !IsEnabled;
-					if (const auto controlMap = RE::ControlMap::GetSingleton()) {
-						controlMap->ignoreKeyboardMouse = IsEnabled;
-					}
+				}
+				break;
+			case RE::INPUT_DEVICE::kMouse:
+				if (scan_code > 7)  // middle scroll
+					io.AddMouseWheelEvent(0, button->Value() * (scan_code == 8 ? 1 : -1));
+				else {
+					if (scan_code > 5)
+						scan_code = 5;
+					io.AddMouseButtonEvent(scan_code, button->IsPressed());
 				}
 				break;
 			default:
@@ -157,11 +182,35 @@ RE::BSEventNotifyControl Menu::ProcessEvent(RE::InputEvent* const* a_event, RE::
 	return RE::BSEventNotifyControl::kContinue;
 }
 
+void Menu::Init(IDXGISwapChain* swapchain, ID3D11Device* device, ID3D11DeviceContext* context)
+{
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	auto& imgui_io = ImGui::GetIO();
+
+	imgui_io.ConfigFlags = ImGuiConfigFlags_NavEnableKeyboard;
+	imgui_io.BackendFlags = ImGuiBackendFlags_HasMouseCursors | ImGuiBackendFlags_RendererHasVtxOffset;
+
+	DXGI_SWAP_CHAIN_DESC desc;
+	swapchain->GetDesc(&desc);
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplWin32_Init(desc.OutputWindow);
+	ImGui_ImplDX11_Init(device, context);
+
+}
+
 void Menu::DrawSettings()
 {
 	ImGuiStyle oldStyle = ImGui::GetStyle();
 	SetupImGuiStyle();
 	static bool visible = false;
+
+	// Start the Dear ImGui frame
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
 
 	ImGui::SetNextWindowSize({ 1024, 1024 }, ImGuiCond_Once);
 	ImGui::Begin("Skyrim Community Shaders", &IsEnabled);
@@ -212,17 +261,6 @@ void Menu::DrawSettings()
 
 	if (ImGui::CollapsingHeader("Menu")) {
 		
-		// Check if we're waiting for input
-		if (settingToggleKey) {
-			// Loop over all the keys and check if any of them are pressed
-			for (int i = 0; i < IM_ARRAYSIZE(ImGui::GetIO().KeysDown); i++) {
-				if (ImGui::IsKeyPressed(i)) {
-					// If a key is pressed, set the selected key code and break out of the loop
-					toggleKey = i;
-					break;
-				}
-			}
-		}
 		if (settingToggleKey) 
 		{
 			ImGui::Text("Press any key to set as toggle key...");
@@ -297,6 +335,9 @@ void Menu::DrawSettings()
 	ImGui::End();
 	ImGuiStyle& style = ImGui::GetStyle();
 	style = oldStyle;
+
+	ImGui::Render();
+	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 }
 
 void Menu::DrawOverlay()
@@ -309,7 +350,12 @@ void Menu::DrawOverlay()
 	compiledShaders = shaderCache.GetCompletedTasks();
 	totalShaders = shaderCache.GetTotalTasks();
 
-	if (compiledShaders != totalShaders) {
+	 if (compiledShaders != totalShaders) 
+	 {
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
 		ImGui::SetNextWindowBgAlpha(1);
 		ImGui::SetNextWindowPos(ImVec2(10, 10));
 		if (!ImGui::Begin("ShaderCompilationInfo", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings)) {
@@ -320,6 +366,9 @@ void Menu::DrawOverlay()
 		ImGui::Text("Compiling Shaders: %d / %d", compiledShaders, totalShaders);
 
 		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 	}
 
 	if (IsEnabled)
@@ -354,4 +403,117 @@ const char* Menu::KeyIdToString(uint32_t key)
 	};
 
 	return keyboard_keys_international[key];
+}
+
+#define IM_VK_KEYPAD_ENTER (VK_RETURN + 256)
+const ImGuiKey Menu::VirtualKeyToImGuiKey(WPARAM vkKey)
+{
+	switch (vkKey) {
+		case VK_TAB: return ImGuiKey_Tab;
+		case VK_LEFT: return ImGuiKey_LeftArrow;
+		case VK_RIGHT: return ImGuiKey_RightArrow;
+		case VK_UP: return ImGuiKey_UpArrow;
+		case VK_DOWN: return ImGuiKey_DownArrow;
+		case VK_PRIOR: return ImGuiKey_PageUp;
+		case VK_NEXT: return ImGuiKey_PageDown;
+		case VK_HOME: return ImGuiKey_Home;
+		case VK_END: return ImGuiKey_End;
+		case VK_INSERT: return ImGuiKey_Insert;
+		case VK_DELETE: return ImGuiKey_Delete;
+		case VK_BACK: return ImGuiKey_Backspace;
+		case VK_SPACE: return ImGuiKey_Space;
+		case VK_RETURN: return ImGuiKey_Enter;
+		case VK_ESCAPE: return ImGuiKey_Escape;
+		case VK_OEM_7: return ImGuiKey_Apostrophe;
+		case VK_OEM_COMMA: return ImGuiKey_Comma;
+		case VK_OEM_MINUS: return ImGuiKey_Minus;
+		case VK_OEM_PERIOD: return ImGuiKey_Period;
+		case VK_OEM_2: return ImGuiKey_Slash;
+		case VK_OEM_1: return ImGuiKey_Semicolon;
+		case VK_OEM_PLUS: return ImGuiKey_Equal;
+		case VK_OEM_4: return ImGuiKey_LeftBracket;
+		case VK_OEM_5: return ImGuiKey_Backslash;
+		case VK_OEM_6: return ImGuiKey_RightBracket;
+		case VK_OEM_3: return ImGuiKey_GraveAccent;
+		case VK_CAPITAL: return ImGuiKey_CapsLock;
+		case VK_SCROLL: return ImGuiKey_ScrollLock;
+		case VK_NUMLOCK: return ImGuiKey_NumLock;
+		case VK_SNAPSHOT: return ImGuiKey_PrintScreen;
+		case VK_PAUSE: return ImGuiKey_Pause;
+		case VK_NUMPAD0: return ImGuiKey_Keypad0;
+		case VK_NUMPAD1: return ImGuiKey_Keypad1;
+		case VK_NUMPAD2: return ImGuiKey_Keypad2;
+		case VK_NUMPAD3: return ImGuiKey_Keypad3;
+		case VK_NUMPAD4: return ImGuiKey_Keypad4;
+		case VK_NUMPAD5: return ImGuiKey_Keypad5;
+		case VK_NUMPAD6: return ImGuiKey_Keypad6;
+		case VK_NUMPAD7: return ImGuiKey_Keypad7;
+		case VK_NUMPAD8: return ImGuiKey_Keypad8;
+		case VK_NUMPAD9: return ImGuiKey_Keypad9;
+		case VK_DECIMAL: return ImGuiKey_KeypadDecimal;
+		case VK_DIVIDE: return ImGuiKey_KeypadDivide;
+		case VK_MULTIPLY: return ImGuiKey_KeypadMultiply;
+		case VK_SUBTRACT: return ImGuiKey_KeypadSubtract;
+		case VK_ADD: return ImGuiKey_KeypadAdd;
+		case IM_VK_KEYPAD_ENTER: return ImGuiKey_KeypadEnter;
+		case VK_LSHIFT: return ImGuiKey_LeftShift;
+		case VK_LCONTROL: return ImGuiKey_LeftCtrl;
+		case VK_LMENU: return ImGuiKey_LeftAlt;
+		case VK_LWIN: return ImGuiKey_LeftSuper;
+		case VK_RSHIFT: return ImGuiKey_RightShift;
+		case VK_RCONTROL: return ImGuiKey_RightCtrl;
+		case VK_RMENU: return ImGuiKey_RightAlt;
+		case VK_RWIN: return ImGuiKey_RightSuper;
+		case VK_APPS: return ImGuiKey_Menu;
+		case '0': return ImGuiKey_0;
+		case '1': return ImGuiKey_1;
+		case '2': return ImGuiKey_2;
+		case '3': return ImGuiKey_3;
+		case '4': return ImGuiKey_4;
+		case '5': return ImGuiKey_5;
+		case '6': return ImGuiKey_6;
+		case '7': return ImGuiKey_7;
+		case '8': return ImGuiKey_8;
+		case '9': return ImGuiKey_9;
+		case 'A': return ImGuiKey_A;
+		case 'B': return ImGuiKey_B;
+		case 'C': return ImGuiKey_C;
+		case 'D': return ImGuiKey_D;
+		case 'E': return ImGuiKey_E;
+		case 'F': return ImGuiKey_F;
+		case 'G': return ImGuiKey_G;
+		case 'H': return ImGuiKey_H;
+		case 'I': return ImGuiKey_I;
+		case 'J': return ImGuiKey_J;
+		case 'K': return ImGuiKey_K;
+		case 'L': return ImGuiKey_L;
+		case 'M': return ImGuiKey_M;
+		case 'N': return ImGuiKey_N;
+		case 'O': return ImGuiKey_O;
+		case 'P': return ImGuiKey_P;
+		case 'Q': return ImGuiKey_Q;
+		case 'R': return ImGuiKey_R;
+		case 'S': return ImGuiKey_S;
+		case 'T': return ImGuiKey_T;
+		case 'U': return ImGuiKey_U;
+		case 'V': return ImGuiKey_V;
+		case 'W': return ImGuiKey_W;
+		case 'X': return ImGuiKey_X;
+		case 'Y': return ImGuiKey_Y;
+		case 'Z': return ImGuiKey_Z;
+		case VK_F1: return ImGuiKey_F1;
+		case VK_F2: return ImGuiKey_F2;
+		case VK_F3: return ImGuiKey_F3;
+		case VK_F4: return ImGuiKey_F4;
+		case VK_F5: return ImGuiKey_F5;
+		case VK_F6: return ImGuiKey_F6;
+		case VK_F7: return ImGuiKey_F7;
+		case VK_F8: return ImGuiKey_F8;
+		case VK_F9: return ImGuiKey_F9;
+		case VK_F10: return ImGuiKey_F10;
+		case VK_F11: return ImGuiKey_F11;
+		case VK_F12: return ImGuiKey_F12;
+		default:
+			return ImGuiKey_None;
+	};
 }
