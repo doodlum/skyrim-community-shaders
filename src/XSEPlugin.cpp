7cf5513833b08fafc79de5955071ff10f9937dbb
Tim <15017472+doodlum@users.noreply.github.com>
2023-05-15 00:23:47 +0100

    Error logging and cleanup

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -6,49 +6,62 @@
 
 #include "ENB/ENBSeriesAPI.h"
 
-extern "C" DLLEXPORT const char* NAME = "Skyrim Community Shaders";
-extern "C" DLLEXPORT const char* DESCRIPTION = "";
-
-HMODULE m_hModule;
-
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID)
-{
-	if (dwReason == DLL_PROCESS_ATTACH)
-		m_hModule = hModule;
-	return TRUE;
-}
+std::list<std::string> errors;
 
 void MessageHandler(SKSE::MessagingInterface::Message* message)
 {
 	switch (message->type) {
 	case SKSE::MessagingInterface::kPostPostLoad:
 		{
-			Hooks::Install();
+			const std::array dlls = {
+				L"ShaderTools.dll",
+				L"SSEShaderTools.dll"
+			};
 
-			auto& shaderCache = SIE::ShaderCache::Instance();
+			for (const auto dll : dlls) {
+				if (GetModuleHandle(dll)) {
+					auto errorMessage = std::format("Incompatible DLL {} detected", stl::utf16_to_utf8(dll).value_or("<unicode conversion error>"s));
+					logger::error("{}", errorMessage);
 
-			shaderCache.SetEnabled(true);
-			shaderCache.SetAsync(true);
-			shaderCache.SetDiskCache(true);
+					errors.push_back(errorMessage);
+				}
+			}
 
-			State::GetSingleton()->Load();
+			if (errors.empty()) {
+				Hooks::Install();
 
-			shaderCache.ValidateDiskCache();
+				auto& shaderCache = SIE::ShaderCache::Instance();
+
+				shaderCache.SetEnabled(true);
+				shaderCache.SetAsync(true);
+				shaderCache.SetDiskCache(true);
+
+				State::GetSingleton()->Load();
+
+				shaderCache.ValidateDiskCache();
+			}
 
 			break;
 		}
 	case SKSE::MessagingInterface::kDataLoaded:
 		{
-			RE::BSInputDeviceManager::GetSingleton()->AddEventSink(Menu::GetSingleton());
-
-			auto& shaderCache = SIE::ShaderCache::Instance();
-
-			while (shaderCache.GetCompletedTasks() != shaderCache.GetTotalTasks()) {
-				std::this_thread::sleep_for(100ms);
+			for (auto it = errors.begin(); it != errors.end(); ++it) {
+				auto& errorMessage = *it;
+				RE::DebugMessageBox(std::format("Community Shaders\n{}, will disable all hooks and features", errorMessage).c_str());
 			}
 
-			if (shaderCache.IsDiskCache()) {
-				shaderCache.WriteDiskCacheInfo();
+			if (errors.empty()) {
+				RE::BSInputDeviceManager::GetSingleton()->AddEventSink(Menu::GetSingleton());
+
+				auto& shaderCache = SIE::ShaderCache::Instance();
+
+				while (shaderCache.GetCompletedTasks() != shaderCache.GetTotalTasks()) {
+					std::this_thread::sleep_for(100ms);
+				}
+
+				if (shaderCache.IsDiskCache()) {
+					shaderCache.WriteDiskCacheInfo();
+				}
 			}
 
 			break;
