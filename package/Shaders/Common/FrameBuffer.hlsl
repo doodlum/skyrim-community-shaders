fe548ca0607767b6251b1876c56a4001adf15835
Alan Tse <alandtse@gmail.com>
2023-07-17 21:08:57 -0700

    Refactor hlsi into common functions
    Simplify Flat and VR code with helper functions. NG_mul has been added
    to replace mul when accessing FrameBuffer content. Additionally code
    from ShadowPS.hlsl was moved to Framebuffer.hlsl.

--- package/Shaders/Common/FrameBuffer.hlsl
+++ package/Shaders/Common/FrameBuffer.hlsl
@@ -1,5 +1,7 @@
+#include "Common/VR.hlsl"
 cbuffer PerFrame : register(b12)
 {
+#if !defined(VR)
 	row_major float4x4 CameraView : packoffset(c0);
 	row_major float4x4 CameraProj : packoffset(c4);
 	row_major float4x4 CameraViewProj : packoffset(c8);
@@ -15,6 +17,23 @@ cbuffer PerFrame : register(b12)
 	float4 FrameParams : packoffset(c42);               // inverse fGamma in x, some flags in yzw
 	float4 DynamicResolutionParams1 : packoffset(c43);  // fDynamicResolutionWidthRatio in x, fDynamicResolutionHeightRatio in y, fDynamicResolutionPreviousWidthRatio in z, fDynamicResolutionPreviousHeightRatio in w
 	float4 DynamicResolutionParams2 : packoffset(c44);  // inverse fDynamicResolutionWidthRatio in x, inverse fDynamicResolutionHeightRatio in y, fDynamicResolutionWidthRatio - fDRClampOffset in z, fDynamicResolutionPreviousWidthRatio - fDRClampOffset in w
+#else
+	row_major float4x4 CameraView : packoffset(c0);
+	row_major float4x4 CameraProj : packoffset(c8);
+	row_major float4x4 CameraViewProj : packoffset(c16);
+	row_major float4x4 CameraViewProjUnjittered : packoffset(c24);
+	row_major float4x4 CameraPreviousViewProjUnjittered : packoffset(c32);
+	row_major float4x4 CameraProjUnjittered : packoffset(c40);
+	row_major float4x4 CameraProjUnjitteredInverse : packoffset(c44);
+	row_major float4x4 CameraViewInverse : packoffset(c56);
+	row_major float4x4 CameraViewProjInverse : packoffset(c64);
+	row_major float4x4 CameraProjInverse : packoffset(c72);
+	float4 CameraPosAdjust : packoffset(c80);
+	float4 CameraPreviousPosAdjust : packoffset(c82);   // fDRClampOffset in w
+	float4 FrameParams : packoffset(c84);               // inverse fGamma in x, some flags in yzw
+	float4 DynamicResolutionParams1 : packoffset(c85);  // fDynamicResolutionWidthRatio in x, fDynamicResolutionHeightRatio in y, fDynamicResolutionPreviousWidthRatio in z, fDynamicResolutionPreviousHeightRatio in w
+	float4 DynamicResolutionParams2 : packoffset(c86);  // inverse fDynamicResolutionWidthRatio in x, inverse fDynamicResolutionHeightRatio in y, fDynamicResolutionWidthRatio - fDRClampOffset in z, fDynamicResolutionPreviousWidthRatio - fDRClampOffset in w
+#endif                                                  // !VR
 }
 
 float2 GetDynamicResolutionAdjustedScreenPosition(float2 screenPosition)
@@ -33,4 +52,17 @@ float2 GetPreviousDynamicResolutionAdjustedScreenPosition(float2 screenPosition)
 float3 ToSRGBColor(float3 linearColor)
 {
 	return pow(linearColor, FrameParams.x);
+}
+
+float3 WorldToView(float3 x, bool is_position = true, uint a_eyeIndex = 0)
+{
+	float4 newPosition = float4(x, (float)is_position);
+	return NG_mul(CameraView, newPosition, a_eyeIndex).xyz;
+}
+
+float2 ViewToUV(float3 x, bool is_position = true, uint a_eyeIndex = 0)
+{
+	float4 newPosition = float4(x, (float)is_position);
+	float4 uv = NG_mul(CameraProj, newPosition, a_eyeIndex);
+	return (uv.xy / uv.w) * float2(0.5f, -0.5f) + 0.5f;
 }
--- package/Shaders/Common/MotionBlur.hlsl
+++ package/Shaders/Common/MotionBlur.hlsl
@@ -1,8 +1,13 @@
-float2 GetSSMotionVector(float4 wsPosition, float4 previousWSPosition)
+float2 GetSSMotionVector(float4 wsPosition, float4 previousWSPosition, uint eyeOffset = 0)
 {
-	float4 screenPosition = mul(CameraViewProjUnjittered, wsPosition);
+	float4 screenPosition = NG_mul(CameraViewProjUnjittered, wsPosition, eyeOffset);
+	float4 previousScreenPosition = NG_mul(CameraPreviousViewProjUnjittered, previousWSPosition, eyeOffset);
+#if !defined(VR)
 	screenPosition.xy = screenPosition.xy / screenPosition.ww;
-	float4 previousScreenPosition = mul(CameraPreviousViewProjUnjittered, previousWSPosition);
 	previousScreenPosition.xy = previousScreenPosition.xy / previousScreenPosition.ww;
+#else
+	screenPosition.xy = screenPosition.xy / screenPosition.zz;
+	previousScreenPosition.xy = previousScreenPosition.xy / previousScreenPosition.zz;
+#endif  // !VR
 	return float2(-0.5, 0.5) * (screenPosition.xy - previousScreenPosition.xy);
 }
