abe074dc321caa550b7bfd74944aaa21b09b413f
Alan Tse <alandtse@gmail.com>
2023-07-22 00:05:01 -0700

    fix: fix bad division for VR in GetSSMotionVector
    The VR code used a different set of disassembly which made it appear the
    divisor had to be different. This now uses the correct w component

--- package/Shaders/Common/MotionBlur.hlsl
+++ package/Shaders/Common/MotionBlur.hlsl
@@ -2,12 +2,7 @@ float2 GetSSMotionVector(float4 a_wsPosition, float4 a_previousWSPosition, uint 
 {
 	float4 screenPosition = mul(CameraViewProjUnjittered[a_eyeIndex], a_wsPosition);
 	float4 previousScreenPosition = mul(CameraPreviousViewProjUnjittered[a_eyeIndex], a_previousWSPosition);
-#if !defined(VR)
 	screenPosition.xy = screenPosition.xy / screenPosition.ww;
 	previousScreenPosition.xy = previousScreenPosition.xy / previousScreenPosition.ww;
-#else
-	screenPosition.xy = screenPosition.xy / screenPosition.zz;
-	previousScreenPosition.xy = previousScreenPosition.xy / previousScreenPosition.zz;
-#endif  // !VR
 	return float2(-0.5, 0.5) * (screenPosition.xy - previousScreenPosition.xy);
 }
