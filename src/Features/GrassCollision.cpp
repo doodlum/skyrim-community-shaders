148aea3fc061a8c8dff010c424d44af5a8600118
Alan Tse <alandtse@users.noreply.github.com>
2023-09-10 03:25:24 -0700

    feat: add NPC grass collision (#79)
    * fix: fix bug where collisionCount always 0
    
    It now will remember the last value from the prior frame.
    
    * feat: add npc grass collision
    
    Allows NPCs to cause grass collisions. This is more costly than the
    player only version. However, additional settings have been added to
    tune quality over performance. We should ask testers for good defaults.
    
    The commit currently enables NPC collisions by default. We can disable
    with maxDistance set to 0.
    
    * style: 🎨 apply clang-format changes
    
    * Some polishing of the Feature menus
    
    * style: 🎨 apply clang-format changes
    
    * chore: convert to popups in menu
    
    ---------
    
    Co-authored-by: TheRiverwoodModder <125157333+TheRiverwoodModder@users.noreply.github.com>
    Co-authored-by: TheRiverwoodModder <TheRiverwoodModder@users.noreply.github.com>

--- src/Features/GrassCollision.cpp
+++ src/Features/GrassCollision.cpp
@@ -31,7 +31,16 @@ void GrassCollision::DrawSettings()
 		if (ImGui::IsItemHovered()) {
 			ImGui::BeginTooltip();
 			ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
-			ImGui::Text("Distance from collision centres to apply collision");
+			ImGui::Text("Distance from collision centres to apply collision.");
+			ImGui::PopTextWrapPos();
+			ImGui::EndTooltip();
+		}
+
+		ImGui::SliderFloat("Max Distance from Player", &settings.maxDistance, 0.0f, 1500.0f);
+		if (ImGui::IsItemHovered()) {
+			ImGui::BeginTooltip();
+			ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+			ImGui::Text("Distance from player to apply collision (NPCs). 0 to disable NPC collisions.");
 			ImGui::PopTextWrapPos();
 			ImGui::EndTooltip();
 		}
@@ -45,6 +54,23 @@ void GrassCollision::DrawSettings()
 			ImGui::EndTooltip();
 		}
 
+		if (ImGui::SliderInt("Calculation Frame Interval", (int*)&settings.frameInterval, 0, 30)) {
+			if (settings.frameInterval)  // increment so mod math works (e.g., skip 1 frame means frame % 2).
+				settings.frameInterval++;
+		}
+		if (ImGui::IsItemHovered()) {
+			ImGui::BeginTooltip();
+			ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+			ImGui::Text("How many frames to skip before calculating positions again. 0 means calculate every frame (most smooth/costly).");
+			ImGui::PopTextWrapPos();
+			ImGui::EndTooltip();
+		}
+
+		ImGui::TreePop();
+	}
+	if (ImGui::TreeNodeEx("Statistics", ImGuiTreeNodeFlags_DefaultOpen)) {
+		ImGui::Text(std::format("Active/Total Actors : {}/{}", activeActorCount, totalActorCount).c_str());
+		ImGui::Text(std::format("Total Collisions : {}", currentCollisionCount).c_str());
 		ImGui::TreePop();
 	}
 }
@@ -131,38 +157,67 @@ void GrassCollision::UpdateCollisions()
 {
 	auto state = RE::BSGraphics::RendererShadowState::GetSingleton();
 
-	std::uint32_t currentCollisionCount = 0;
+	auto frameCount = RE::BSGraphics::State::GetSingleton()->uiFrameCount;
 
-	std::vector<CollisionSData> collisionsData{};
-
-	if (auto player = RE::PlayerCharacter::GetSingleton()) {
-		if (auto root = player->Get3D(false)) {
-			auto position = player->GetPosition();
-			RE::BSVisit::TraverseScenegraphCollision(root, [&](RE::bhkNiCollisionObject* a_object) -> RE::BSVisit::BSVisitControl {
-				RE::NiPoint3 centerPos;
-				float radius;
-				if (GetShapeBound(a_object, centerPos, radius)) {
-					radius *= settings.RadiusMultiplier;
-					CollisionSData data{};
-					RE::NiPoint3 eyePosition{};
-					for (int eyeIndex = 0; eyeIndex < eyeCount; eyeIndex++) {
-						if (!REL::Module::IsVR()) {
-							eyePosition = state->GetRuntimeData().posAdjust.getEye();
-						} else
-							eyePosition = state->GetVRRuntimeData().posAdjust.getEye(eyeIndex);
-						data.centre[eyeIndex].x = centerPos.x - eyePosition.x;
-						data.centre[eyeIndex].y = centerPos.y - eyePosition.y;
-						data.centre[eyeIndex].z = centerPos.z - eyePosition.z;
+	if (settings.frameInterval == 0 || frameCount % settings.frameInterval == 0) {  // only calculate actor positions on some frames
+		currentCollisionCount = 0;
+		totalActorCount = 0;
+		activeActorCount = 0;
+		actorList.clear();
+		collisionsData.clear();
+		// actor query code from po3 under MIT
+		// https://github.com/powerof3/PapyrusExtenderSSE/blob/7a73b47bc87331bec4e16f5f42f2dbc98b66c3a7/include/Papyrus/Functions/Faction.h#L24C7-L46
+		if (const auto processLists = RE::ProcessLists::GetSingleton(); processLists && settings.maxDistance > 0.0f) {
+			std::vector<RE::BSTArray<RE::ActorHandle>*> actors;
+			actors.push_back(&processLists->highActorHandles);  // high actors are in combat or doing something interesting
+			for (auto array : actors) {
+				for (auto& actorHandle : *array) {
+					auto actorPtr = actorHandle.get();
+					if (actorPtr && actorPtr.get() && actorPtr.get()->Is3DLoaded()) {
+						actorList.push_back(actorPtr.get());
+						totalActorCount++;
 					}
-					data.radius = radius;
-					currentCollisionCount++;
-					collisionsData.push_back(data);
 				}
-				return RE::BSVisit::BSVisitControl::kContinue;
-			});
+			}
+		}
+
+		RE::NiPoint3 playerPosition;
+		if (auto player = RE::PlayerCharacter::GetSingleton()) {
+			actorList.push_back(player);
+			playerPosition = player->GetPosition();
+		}
+
+		for (const auto actor : actorList) {
+			if (auto root = actor->Get3D(false)) {
+				if (playerPosition.GetDistance(actor->GetPosition()) > settings.maxDistance) {  // npc too far so skip
+					continue;
+				}
+				activeActorCount++;
+				RE::BSVisit::TraverseScenegraphCollision(root, [&](RE::bhkNiCollisionObject* a_object) -> RE::BSVisit::BSVisitControl {
+					RE::NiPoint3 centerPos;
+					float radius;
+					if (GetShapeBound(a_object, centerPos, radius)) {
+						radius *= settings.RadiusMultiplier;
+						CollisionSData data{};
+						RE::NiPoint3 eyePosition{};
+						for (int eyeIndex = 0; eyeIndex < eyeCount; eyeIndex++) {
+							if (!REL::Module::IsVR()) {
+								eyePosition = state->GetRuntimeData().posAdjust.getEye();
+							} else
+								eyePosition = state->GetVRRuntimeData().posAdjust.getEye(eyeIndex);
+							data.centre[eyeIndex].x = centerPos.x - eyePosition.x;
+							data.centre[eyeIndex].y = centerPos.y - eyePosition.y;
+							data.centre[eyeIndex].z = centerPos.z - eyePosition.z;
+						}
+						data.radius = radius;
+						currentCollisionCount++;
+						collisionsData.push_back(data);
+					}
+					return RE::BSVisit::BSVisitControl::kContinue;
+				});
+			}
 		}
 	}
-
 	if (!currentCollisionCount) {
 		CollisionSData data{};
 		ZeroMemory(&data, sizeof(data));
@@ -170,7 +225,6 @@ void GrassCollision::UpdateCollisions()
 		currentCollisionCount = 1;
 	}
 
-	static std::uint32_t colllisionCount = 0;
 	bool collisionCountChanged = currentCollisionCount != colllisionCount;
 
 	if (!collisions || collisionCountChanged) {
