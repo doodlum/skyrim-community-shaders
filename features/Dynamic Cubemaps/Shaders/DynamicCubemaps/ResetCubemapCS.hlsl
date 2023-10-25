662685575d2ecffd40cdece7e258ad988e62ce42
Tim <15017472+doodlum@users.noreply.github.com>
2023-10-25 02:39:07 +0100

    chore: remove unused dynamic cubemap shaders

--- features/Dynamic Cubemaps/Shaders/DynamicCubemaps/ResetCubemapCS.hlsl
+++
@@ -1,7 +0,0 @@
-RWTexture2DArray<float4> OutputTexture : register(u0);
-
-[numthreads(32, 32, 1)]
-void main(uint3 ThreadID : SV_DispatchThreadID)
-{	
-    OutputTexture[ThreadID] = float4(0, 0, 0, 0);   
-}
