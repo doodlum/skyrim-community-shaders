a707e5230a5160d65d128096e2743da9e4304215
doodlum <15017472+doodlum@users.noreply.github.com>
2024-01-31 22:03:33 +0000

    Revert "refactor: move RunGrass and DistantTree out of individual features and into main packages"

--- package/Shaders/DistantTree.hlsl
+++ package/Shaders/DistantTree.hlsl
@@ -1,41 +1,24 @@
-#include "Common/Color.hlsl"
-#include "Common/FrameBuffer.hlsl"
-#include "Common/MotionBlur.hlsl"
-
-cbuffer PerFrame : register(b3)
-{
-	row_major float3x4 DirectionalAmbient;
-	float4 DirLightColor;
-	float4 DirLightDirection;
-	float DirLightScale;
-	bool ComplexAtlasTexture;
-	bool EnableComplexTreeLOD;
-	bool EnableDirLightFix;
-	float SubsurfaceScatteringAmount;
-	float pad[3];
-}
-
 struct VS_INPUT
 {
 	float3 Position : POSITION0;
 	float2 TexCoord0 : TEXCOORD0;
 	float4 InstanceData1 : TEXCOORD4;
 	float4 InstanceData2 : TEXCOORD5;
-	float4 InstanceData3 : TEXCOORD6;  // Unused
-	float4 InstanceData4 : TEXCOORD7;  // Unused
+	float4 InstanceData3 : TEXCOORD6;
+	float4 InstanceData4 : TEXCOORD7;
 };
 
 struct VS_OUTPUT
 {
 	float4 Position : SV_POSITION0;
 	float3 TexCoord : TEXCOORD0;
+
 #if defined(RENDER_DEPTH)
 	float4 Depth : TEXCOORD3;
 #else
 	float4 WorldPosition : POSITION1;
 	float4 PreviousWorldPosition : POSITION2;
 #endif
-	float3 SphereNormal : TEXCOORD4;
 };
 
 #ifdef VSHADER
@@ -60,7 +43,6 @@ VS_OUTPUT main(VS_INPUT input)
 	adjustedModelPosition.x = dot(float2(1, -1) * input.InstanceData2.xy, scaledModelPosition.xy);
 	adjustedModelPosition.y = dot(input.InstanceData2.yx, scaledModelPosition.xy);
 	adjustedModelPosition.z = scaledModelPosition.z;
-
 	float4 finalModelPosition = float4(input.InstanceData1.xyz + adjustedModelPosition.xyz, 1.0);
 	float4 viewPosition = mul(WorldViewProj, finalModelPosition);
 
@@ -74,13 +56,6 @@ VS_OUTPUT main(VS_INPUT input)
 
 	vsout.Position = viewPosition;
 	vsout.TexCoord = float3(input.TexCoord0.xy, FogParam.z);
-
-	scaledModelPosition = input.Position.xyz;
-	adjustedModelPosition.x = dot(float2(1, -1) * input.InstanceData2.xy, scaledModelPosition.xy);
-	adjustedModelPosition.y = dot(input.InstanceData2.yx, scaledModelPosition.xy);
-	adjustedModelPosition.z = scaledModelPosition.z;
-
-	vsout.SphereNormal.xyz = mul(World, normalize(adjustedModelPosition));
 
 	return vsout;
 }
@@ -100,12 +75,19 @@ struct PS_OUTPUT
 
 #ifdef PSHADER
 SamplerState SampDiffuse : register(s0);
-SamplerState SampShadowMaskSampler : register(s14);
+
 Texture2D<float4> TexDiffuse : register(t0);
 
 cbuffer AlphaTestRefCB : register(b11)
 {
 	float AlphaTestRefRS : packoffset(c0);
+}
+
+cbuffer PerFrame : register(b12)
+{
+	float4 UnknownPerFrame1[12] : packoffset(c0);
+	row_major float4x4 ScreenProj : packoffset(c12);
+	row_major float4x4 PreviousScreenProj : packoffset(c16);
 }
 
 cbuffer PerTechnique : register(b0)
@@ -133,49 +115,7 @@ const static float DepthOffsets[16] = {
 	0.333333343
 };
 
-float GetSoftLightMultiplier(float angle, float strength)
-{
-	float softLightParam = saturate((strength + angle) / (1 + strength));
-	float arg1 = (softLightParam * softLightParam) * (3 - 2 * softLightParam);
-	float clampedAngle = saturate(angle);
-	float arg2 = (clampedAngle * clampedAngle) * (3 - 2 * clampedAngle);
-	float softLigtMul = saturate(arg1 - arg2);
-	return softLigtMul;
-}
-
-float3 TransformNormal(float3 normal)
-{
-	return normal * 2 + -1.0.xxx;
-}
-
-// http://www.thetenthplanet.de/archives/1180
-float3x3 CalculateTBN(float3 N, float3 p, float2 uv)
-{
-	// get edge vectors of the pixel triangle
-	float3 dp1 = ddx_coarse(p);
-	float3 dp2 = ddy_coarse(p);
-	float2 duv1 = ddx_coarse(uv);
-	float2 duv2 = ddy_coarse(uv);
-
-	// solve the linear system
-	float3 dp2perp = cross(dp2, N);
-	float3 dp1perp = cross(N, dp1);
-	float3 T = dp2perp * duv1.x + dp1perp * duv2.x;
-	float3 B = dp2perp * duv1.y + dp1perp * duv2.y;
-
-	// construct a scale-invariant frame
-	float invmax = rsqrt(max(dot(T, T), dot(B, B)));
-	return float3x3(T * invmax, B * invmax, N);
-}
-
-Texture2D<float4> TexShadowMaskSampler : register(t17);
-
-#	if defined(SCREEN_SPACE_SHADOWS)
-#		include "ScreenSpaceShadows/ShadowsPS.hlsli"
-#	endif
-
-PS_OUTPUT main(PS_INPUT input, bool frontFace
-			   : SV_IsFrontFace)
+PS_OUTPUT main(PS_INPUT input)
 {
 	PS_OUTPUT psout;
 
@@ -199,7 +139,6 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace
 	psout.Albedo.xyz = input.Depth.xxx / input.Depth.yyy;
 	psout.Albedo.w = 0;
 #	else
-
 	float4 baseColor = TexDiffuse.Sample(SampDiffuse, input.TexCoord.xy);
 
 #		if defined(DO_ALPHA_TEST)
@@ -208,73 +147,17 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace
 	}
 #		endif
 
-	float2 screenMotionVector = GetSSMotionVector(input.WorldPosition, input.PreviousWorldPosition);
+	psout.Albedo = float4((input.TexCoord.zzz * DiffuseColor.xyz + AmbientColor.xyz) * baseColor.xyz, 1.0);
 
-	float shadowColor = TexShadowMaskSampler.Load(int3(input.Position.xy, 0));
+	float4 screenPosition = mul(ScreenProj, input.WorldPosition);
+	screenPosition.xy = screenPosition.xy / screenPosition.ww;
+	float4 previousScreenPosition = mul(PreviousScreenProj, input.PreviousWorldPosition);
+	previousScreenPosition.xy = previousScreenPosition.xy / previousScreenPosition.ww;
+	float2 screenMotionVector = float2(-0.5, 0.5) * (screenPosition.xy - previousScreenPosition.xy);
 
 	psout.MotionVector = screenMotionVector;
 
-	float3 ddx = ddx_coarse(input.WorldPosition);
-	float3 ddy = ddy_coarse(input.WorldPosition);
-	float3 normal = normalize(cross(ddx, ddy));
-
-	float3 viewDirection = -normalize(input.WorldPosition.xyz);
-	float3 worldNormal = normal;
-
-	worldNormal = normalize(input.SphereNormal.xyz);
-	worldNormal.xy *= 2;
-	worldNormal = normalize(worldNormal);
-	worldNormal = normalize(lerp(-worldNormal, normal, 0.25));
-
-	if (ComplexAtlasTexture && EnableComplexTreeLOD) {
-		float3 normalColor = TexDiffuse.Sample(SampDiffuse, float2(input.TexCoord.x, 0.5 + input.TexCoord.y));
-		normalColor = TransformNormal(normalColor);
-		// Increases the strength of the normal to simulate more advanced lighting.
-		normalColor.xy *= 2;
-		normalColor = normalize(normalColor);
-		// world-space -> tangent-space -> world-space.
-		// This is because we don't have pre-computed tangents.
-		worldNormal.xyz = normalize(mul(normalColor.xyz, CalculateTBN(worldNormal.xyz, -input.WorldPosition.xyz, input.TexCoord.xy)));
-	}
-
-	float3 dirLightColor = lerp(RGBToLuminance(DirLightColor.xyz), DirLightColor.xyz, 0.5) * 0.5;
-
-	if (EnableDirLightFix) {
-		dirLightColor *= DirLightScale;
-	}
-
-	float3 nsDirLightColor = dirLightColor;
-
-#		if defined(SCREEN_SPACE_SHADOWS)
-	float dirLightSShadow = PrepassScreenSpaceShadows(input.WorldPosition);
-	shadowColor *= dirLightSShadow;
-#		endif
-
-	float3 diffuseColor = 0;
-
-	float3 lightsDiffuseColor = 0;
-
-	float dirLightAngle = dot(worldNormal.xyz, DirLightDirection.xyz);
-	float3 dirDiffuseColor = dirLightColor * saturate(dirLightAngle);
-
-	lightsDiffuseColor += dirDiffuseColor * dirLightColor;
-
-	float3 subsurfaceColor = lerp(RGBToLuminance(baseColor.xyz), baseColor.xyz, 2.0);
-
-	// Applies lighting across the whole surface apart from what is already lit.
-	lightsDiffuseColor += subsurfaceColor * nsDirLightColor * GetSoftLightMultiplier(dirLightAngle, SubsurfaceScatteringAmount);
-
-	// Applies lighting from the opposite direction. Does not account for normals perpendicular to the light source.
-	lightsDiffuseColor += subsurfaceColor * dirLightColor * saturate(-dirLightAngle) * SubsurfaceScatteringAmount;
-
-	float3 directionalAmbientColor = mul(DirectionalAmbient, float4(worldNormal.xyz, 1));
-	lightsDiffuseColor += directionalAmbientColor;
-
-	diffuseColor += lightsDiffuseColor;
-
-	float3 color = diffuseColor * baseColor.xyz;
-	psout.Albedo.xyz = color;
-	psout.Albedo.w = 1;
+	psout.Normal = float4(0.5, 0.5, 0, 0);
 #	endif
 
 	return psout;
