c37c34fcb264a5777308108c8a670ee187f775e3
doodlum <doodlum@users.noreply.github.com>
2024-02-27 23:48:58 +0000

    style: 🎨 apply clang-format changes

--- features/Subsurface Scattering/Shaders/SubsurfaceScattering/SeparableSSSCS.hlsl
+++ features/Subsurface Scattering/Shaders/SubsurfaceScattering/SeparableSSSCS.hlsl
@@ -3,20 +3,20 @@ RWTexture2D<float4> SSSRW : register(u0);
 Texture2D<float4> ColorTexture : register(t0);
 Texture2D<float4> DepthTexture : register(t1);
 
-#	if defined(HORIZONTAL)
+#if defined(HORIZONTAL)
 Texture2D<float4> NormalTexture : register(t2);
-#	else
+#else
 RWTexture2D<unorm float4> NormalTexture : register(u1);
-#	endif
+#endif
 
 cbuffer PerFrame : register(b0)
 {
-    float4 DynamicRes;
+	float4 DynamicRes;
 	float4 CameraData;
 	float2 BufferDim;
-    float2 RcpBufferDim;
+	float2 RcpBufferDim;
 	uint FrameCount;
-    float SSSS_FOVY;
+	float SSSS_FOVY;
 };
 
 float GetScreenDepth(float depth)
@@ -26,19 +26,20 @@ float GetScreenDepth(float depth)
 
 #include "SeparableSSS.hlsli"
 
-[numthreads(32, 32, 1)] void main(uint3 DTid : SV_DispatchThreadID) {
+[numthreads(32, 32, 1)] void main(uint3 DTid
+								  : SV_DispatchThreadID) {
 	float2 texCoord = (DTid.xy + 0.5) * RcpBufferDim;
-#	if defined(HORIZONTAL)
-		float4 normals = NormalTexture[DTid.xy];
-		float4 color = SSSSBlurCS(DTid.xy, texCoord, float2(0.0, 1.0), normals);
-	    SSSRW[DTid.xy] = color;
-#	else
-		float4 normals = NormalTexture[DTid.xy];
-	    float4 color = SSSSBlurCS(DTid.xy, texCoord, float2(0.0, 1.0), normals);
-		color.rgb = Lin2sRGB(color.rgb);		
+#if defined(HORIZONTAL)
+	float4 normals = NormalTexture[DTid.xy];
+	float4 color = SSSSBlurCS(DTid.xy, texCoord, float2(0.0, 1.0), normals);
+	SSSRW[DTid.xy] = color;
+#else
+	float4 normals = NormalTexture[DTid.xy];
+	float4 color = SSSSBlurCS(DTid.xy, texCoord, float2(0.0, 1.0), normals);
+	color.rgb = Lin2sRGB(color.rgb);
 
-		SSSRW[DTid.xy] = color;
+	SSSRW[DTid.xy] = color;
 
-		NormalTexture[DTid.xy] = float4(normals.xy, normals.z < 1 ? normals.z : 0.0, normals.w);
-#	endif
+	NormalTexture[DTid.xy] = float4(normals.xy, normals.z < 1 ? normals.z : 0.0, normals.w);
+#endif
 }
--- src/Features/SubsurfaceScattering.h
+++ src/Features/SubsurfaceScattering.h
@@ -93,5 +93,4 @@ public:
 			logger::info("[SSS] Installed hooks");
 		}
 	};
-
 };
