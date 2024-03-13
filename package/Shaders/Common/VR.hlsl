72a303189540a602a543256ca165b03bc9a0ef3c
Alan Tse <alandtse@gmail.com>
2024-03-12 23:37:55 -0700

    chore: add convertostereosp to vr.hlsl

--- package/Shaders/Common/VR.hlsl
+++ package/Shaders/Common/VR.hlsl
@@ -1,7 +1,7 @@
 /**
-Converts to the eye specific uv.
+Converts to the eye specific uv [0,1].
 In VR, texture buffers include the left and right eye in the same buffer. Flat only has a single camera for the entire width.
-This means the x value [0, .5] represents the left eye, and the y value (.5, 1] are the right eye.
+This means the x value [0, .5] represents the left eye, and the x value (.5, 1] are the right eye.
 This returns the adjusted value
 @param uv - uv coords [0,1] to be encoded for VR
 @param a_eyeIndex The eyeIndex; 0 is left, 1 is right
@@ -17,9 +17,9 @@ float2 ConvertToStereoUV(float2 uv, uint a_eyeIndex)
 }
 
 /**
-Converts from eye specific uv to general uv.
+Converts from eye specific uv to general uv [0,1].
 In VR, texture buffers include the left and right eye in the same buffer. 
-This means the x value [0, .5] represents the left eye, and the y value (.5, 1] are the right eye.
+This means the x value [0, .5] represents the left eye, and the x value (.5, 1] are the right eye.
 This returns the adjusted value
 @param uv - eye specific uv coords [0,1]; if uv.x < 0.5, it's a left eye; otherwise right
 @param a_eyeIndex The eyeIndex; 0 is left, 1 is right
@@ -32,4 +32,20 @@ float2 ConvertFromStereoUV(float2 uv, uint a_eyeIndex)
 	uv.x = 2 * uv.x - (float)a_eyeIndex;
 #endif
 	return uv;
+}
+
+/**
+Converts to the eye specific screenposition [0,Resolution].
+In VR, texture buffers include the left and right eye in the same buffer. Flat only has a single camera for the entire width.
+This means the x value [0, resx/2] represents the left eye, and the x value (resx/2, x] are the right eye.
+This returns the adjusted value
+@param screenPosition - Screenposition coords ([0,resx], [0,resy]) to be encoded for VR
+@param a_eyeIndex The eyeIndex; 0 is left, 1 is right
+@param a_resolution The resolution of the screen
+@returns screenPosition with x coords adjusted for the VR texture buffer
+*/
+float2 ConvertToStereoSP(float2 screenPosition, uint a_eyeIndex, float2 a_resolution)
+{
+	screenPosition.x /= a_resolution.x;
+	return ConvertToStereoUV(screenPosition, a_eyeIndex) * a_resolution;
 }
