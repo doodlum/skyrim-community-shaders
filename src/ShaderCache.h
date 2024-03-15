bfc4679247c3738b184ba82b6aaa64759bab4c98
Tim <15017472+doodlum@users.noreply.github.com>
2024-03-15 17:31:25 +0000

    chore: update version numbers

--- src/ShaderCache.h
+++ src/ShaderCache.h
@@ -9,7 +9,7 @@
 #include <unordered_map>
 #include <unordered_set>
 
-static constexpr REL::Version SHADER_CACHE_VERSION = { 0, 0, 0, 16 };
+static constexpr REL::Version SHADER_CACHE_VERSION = { 0, 0, 0, 17 };
 
 using namespace std::chrono;
 
