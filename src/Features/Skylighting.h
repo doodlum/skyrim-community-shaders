c9c713596b22eeae5ae970e893c6b80d7f8210f2
Tim <15017472+doodlum@users.noreply.github.com>
2024-04-26 16:56:37 +0100

    feat: occlusion map fixes

--- src/Features/Skylighting.h
+++ src/Features/Skylighting.h
@@ -84,174 +84,212 @@ public:
 	void Bind();
 	void Compute();
 
-	enum class RenderMode : uint32_t
+	enum class ShaderTechnique
 	{
-		Depth = 0xC,
-		SpotShadowmap = 0xD,
-		DirectionalShadowmap = 0xE,
-		OmnidirectionalShadowmap = 0xF,
-		Unk17 = 0x11,
-		LocalMap = 0x12,
-		LocalMapFogOfWar = 0x13,
-		FirstPersonView = 0x16,
-		BloodSplatter = 0x17,
-		DebugColor = 0x1B,
-		PrecipitationOcclusionMap = 0x1C,
+		// Sky
+		SkySunOcclude = 0x2,
+
+		// Grass
+		GrassNoAlphaDirOnlyFlatLit = 0x3,
+		GrassNoAlphaDirOnlyFlatLitSlope = 0x5,
+		GrassNoAlphaDirOnlyVertLitSlope = 0x6,
+		GrassNoAlphaDirOnlyFlatLitBillboard = 0x13,
+		GrassNoAlphaDirOnlyFlatLitSlopeBillboard = 0x14,
+
+		// Utility
+		UtilityGeneralStart = 0x2B,
+
+		// Effect
+		EffectGeneralStart = 0x4000002C,
+
+		// Lighting
+		LightingGeneralStart = 0x4800002D,
+
+		// DistantTree
+		DistantTreeDistantTreeBlock = 0x5C00002E,
+		DistantTreeDepth = 0x5C00002F,
+
+		// Grass
+		GrassDirOnlyFlatLit = 0x5C000030,
+		GrassDirOnlyFlatLitSlope = 0x5C000032,
+		GrassDirOnlyVertLitSlope = 0x5C000033,
+		GrassDirOnlyFlatLitBillboard = 0x5C000040,
+		GrassDirOnlyFlatLitSlopeBillboard = 0x5C000041,
+		GrassRenderDepth = 0x5C00005C,
+
+		// Sky
+		SkySky = 0x5C00005E,
+		SkyMoonAndStarsMask = 0x5C00005F,
+		SkyStars = 0x5C000060,
+		SkyTexture = 0x5C000061,
+		SkyClouds = 0x5C000062,
+		SkyCloudsLerp = 0x5C000063,
+		SkyCloudsFade = 0x5C000064,
+
+		// Particle
+		ParticleParticles = 0x5C000065,
+		ParticleParticlesGryColorAlpha = 0x5C000066,
+		ParticleParticlesGryColor = 0x5C000067,
+		ParticleParticlesGryAlpha = 0x5C000068,
+		ParticleEnvCubeSnow = 0x5C000069,
+		ParticleEnvCubeRain = 0x5C00006A,
+
+		// Water
+		WaterSimple = 0x5C00006B,
+		WaterSimpleVc = 0x5C00006C,
+		WaterStencil = 0x5C00006D,
+		WaterStencilVc = 0x5C00006E,
+		WaterDisplacementStencil = 0x5C00006F,
+		WaterDisplacementStencilVc = 0x5C000070,
+		WaterGeneralStart = 0x5C000071,
+
+		// Sky
+		SkySunGlare = 0x5C006072,
+
+		// BloodSplater
+		BloodSplaterFlare = 0x5C006073,
+		BloodSplaterSplatter = 0x5C006074,
 	};
 
-	enum class Flags
+	class BSUtilityShader : public RE::BSShader
 	{
-		None = 0,
-		Vc = 1 << 0,
-		Texture = 1 << 1,
-		Skinned = 1 << 2,
-		Normals = 1 << 3,
-		BinormalTangent = 1 << 4,
-		AlphaTest = 1 << 7,
-		LodLandscape = 1 << 8,
-		RenderNormal = 1 << 9,
-		RenderNormalFalloff = 1 << 10,
-		RenderNormalClamp = 1 << 11,
-		RenderNormalClear = 1 << 12,
-		RenderDepth = 1 << 13,
-		RenderShadowmap = 1 << 14,
-		RenderShadowmapClamped = 1 << 15,
-		GrayscaleToAlpha = 1 << 15,
-		RenderShadowmapPb = 1 << 16,
-		AdditionalAlphaMask = 1 << 16,
-		DepthWriteDecals = 1 << 17,
-		DebugShadowSplit = 1 << 18,
-		DebugColor = 1 << 19,
-		GrayscaleMask = 1 << 20,
-		RenderShadowmask = 1 << 21,
-		RenderShadowmaskSpot = 1 << 22,
-		RenderShadowmaskPb = 1 << 23,
-		RenderShadowmaskDpb = 1 << 24,
-		RenderBaseTexture = 1 << 25,
-		TreeAnim = 1 << 26,
-		LodObject = 1 << 27,
-		LocalMapFogOfWar = 1 << 28,
-		OpaqueEffect = 1 << 29,
+	public:
+		enum class Flags
+		{
+			None = 0,
+			Vc = 1 << 0,
+			Texture = 1 << 1,
+			Skinned = 1 << 2,
+			Normals = 1 << 3,
+			BinormalTangent = 1 << 4,
+			AlphaTest = 1 << 7,
+			LodLandscape = 1 << 8,
+			RenderNormal = 1 << 9,
+			RenderNormalFalloff = 1 << 10,
+			RenderNormalClamp = 1 << 11,
+			RenderNormalClear = 1 << 12,
+			RenderDepth = 1 << 13,
+			RenderShadowmap = 1 << 14,
+			RenderShadowmapClamped = 1 << 15,
+			GrayscaleToAlpha = 1 << 15,
+			RenderShadowmapPb = 1 << 16,
+			AdditionalAlphaMask = 1 << 16,
+			DepthWriteDecals = 1 << 17,
+			DebugShadowSplit = 1 << 18,
+			DebugColor = 1 << 19,
+			GrayscaleMask = 1 << 20,
+			RenderShadowmask = 1 << 21,
+			RenderShadowmaskSpot = 1 << 22,
+			RenderShadowmaskPb = 1 << 23,
+			RenderShadowmaskDpb = 1 << 24,
+			RenderBaseTexture = 1 << 25,
+			TreeAnim = 1 << 26,
+			LodObject = 1 << 27,
+			LocalMapFogOfWar = 1 << 28,
+			OpaqueEffect = 1 << 29,
+		};
+
+		static BSUtilityShader* GetSingleton()
+		{
+			REL::Relocation<BSUtilityShader**> singleton{ RELOCATION_ID(528354, 415300) };
+			return *singleton;
+		}
+		~BSUtilityShader() override;  // 00
+
+		// override (BSShader)
+		bool SetupTechnique(std::uint32_t globalTechnique) override;            // 02
+		void RestoreTechnique(std::uint32_t globalTechnique) override;          // 03
+		void SetupGeometry(RE::BSRenderPass* pass, uint32_t flags) override;    // 06
+		void RestoreGeometry(RE::BSRenderPass* pass, uint32_t flags) override;  // 07
 	};
 
-	
-	static void EmplacePass(RE::BSLightingShaderProperty::Data* This, RE::BSShader* shader, RE::BSShaderProperty* property, RE::BSGeometry* geometry, uint32_t technique, uint8_t numLights, RE::BSLight* light0, RE::BSLight* light1, RE::BSLight* light2, RE::BSLight* light3)
+	struct RenderPassArray
 	{
-		using func_t = decltype(&EmplacePass);
-		REL::Relocation<func_t> func{ REL::RelocationID(98884, 25642) };
-		func(This, shader, property, geometry, technique, numLights, light0, light1, light2, light3);
-	}
+		static RE::BSRenderPass* MakeRenderPass(RE::BSShader* shader, RE::BSShaderProperty* property, RE::BSGeometry* geometry,
+			uint32_t technique, uint8_t numLights, RE::BSLight** lights)
+		{
+			using func_t = decltype(&RenderPassArray::MakeRenderPass);
+			REL::Relocation<func_t> func{ RELOCATION_ID(100717, 107497) };
+			return func(shader, property, geometry, technique, numLights, lights);
+		}
 
-	static RE::BSShader* GetUtilityShaderSingleton()
-	{
-		REL::Relocation<RE::BSShader**> singleton{ RELOCATION_ID(528354, 415300) };
-		return *singleton;
-	}
+		static void ClearRenderPass(RE::BSRenderPass* pass)
+		{
+			using func_t = decltype(&RenderPassArray::ClearRenderPass);
+			REL::Relocation<func_t> func{ RELOCATION_ID(100718, 107498) };
+			func(pass);
+		}
 
-	stl::enumeration<RE::BSLightingShaderProperty::EShaderPropertyFlag, std::uint64_t> flagsBackup;
+		void Clear()
+		{
+			while (head != nullptr) {
+				RE::BSRenderPass* next = head->next;
+				ClearRenderPass(head);
+				head = next;
+			}
+			head = nullptr;
+		}
+
+		RE::BSRenderPass* EmplacePass(RE::BSShader* shader, RE::BSShaderProperty* property, RE::BSGeometry* geometry,
+			uint32_t technique, uint8_t numLights = 0, RE::BSLight* light0 = nullptr, RE::BSLight* light1 = nullptr,
+			RE::BSLight* light2 = nullptr, RE::BSLight* light3 = nullptr)
+		{
+			RE::BSLight* lights[4];
+			lights[0] = light0;
+			lights[1] = light1;
+			lights[2] = light2;
+			lights[3] = light3;
+			auto* newPass = MakeRenderPass(shader, property, geometry, technique, numLights, lights);
+			if (head != nullptr) {
+				RE::BSRenderPass* lastPass = head;
+				while (lastPass->next != nullptr) {
+					lastPass = lastPass->next;
+				}
+				lastPass->next = newPass;
+			} else {
+				head = newPass;
+			}
+			return newPass;
+		}
+
+		RE::BSRenderPass* head;  // 00
+		uint64_t unk08;          // 08
+	};
 
 	struct BSLightingShaderProperty_GetPrecipitationOcclusionMapRenderPassesImpl
 	{
-		static RE::BSLightingShaderProperty::Data* thunk(RE::BSLightingShaderProperty* This, RE::BSGeometry* geometry, [[maybe_unused]] RenderMode renderMode, [[maybe_unused]] RE::BSShaderAccumulator* accumulator)
+		static void* thunk(RE::BSLightingShaderProperty* property, RE::BSGeometry* geometry, [[maybe_unused]] uint32_t renderMode, [[maybe_unused]] RE::BSShaderAccumulator* accumulator)
 		{
-			//using enum RE::BSLightingShaderProperty::EShaderPropertyFlag;
-			//using enum Flags;
+			using enum RE::BSShaderProperty::EShaderPropertyFlag;
+			using enum BSUtilityShader::Flags;
 
-			//auto& flags = This->flags;
+			auto* precipitationOcclusionMapRenderPassList = reinterpret_cast<RenderPassArray*>(&property->unk0C8);
 
-			//if (flags.none(kRefraction, kTempRefraction, kZBufferWrite, kSkinned, kMultiTextureLandscape, kNoLODLandBlend, kLODLandscape, kEyeReflect, kDecal, kDynamicDecal, kAnisotropicLighting)) {
-			//	const auto alphaProperty = static_cast<RE::NiAlphaProperty*>(geometry->GetGeometryRuntimeData().properties[0].get());
+			precipitationOcclusionMapRenderPassList->Clear();
+			if (property->flags.any(kZBufferWrite) && property->flags.none(kDecal, kDynamicDecal)) {
+				stl::enumeration<BSUtilityShader::Flags> technique;
+				technique.set(RenderDepth);
 
-			//	stl::enumeration<Flags> technique;
-			//	technique.set(RenderDepth, Texture);
+				if (property->flags.any(kModelSpaceNormals)) {
+					technique.set(Normals, BinormalTangent);
+				}
+				if (property->flags.any(kVertexColors)) {
+					technique.set(Vc);
+				}
+				if (property->flags.any(kLODObjects, kHDLODObjects)) {
+					technique.set(LodObject);
+				}
 
-			//	if (flags.any(kModelSpaceNormals)) {
-			//		technique.set(Normals, BinormalTangent);
-			//	}
-			//	if (flags.any(kVertexColors)) {
-			//		technique.set(Vc);
-			//	}
-			//	if (alphaProperty && alphaProperty->GetAlphaTesting()) {
-			//		technique.set(AlphaTest);
-			//	}
-			//	if (flags.any(kLODObjects, kHDLODObjects)) {
-			//		technique.set(LodObject);
-			//	}
-			//	if (flags.any(kTreeAnim)) {
-			//		technique.set(TreeAnim);
-			//	}
-			//	EmplacePass(&This->unk0C8, GetUtilityShaderSingleton(), This, geometry, technique.underlying() + static_cast<uint32_t>(0x2B), 0, 0, 0, 0, 0);
-			//}
-			//return &This->unk0C8;
-			using enum RE::BSLightingShaderProperty::EShaderPropertyFlag;
-
-			GetSingleton()->flagsBackup = This->flags;
-			This->flags.reset(kRefraction, kTempRefraction, kZBufferWrite, kSkinned, kMultiTextureLandscape, kNoLODLandBlend, kLODLandscape, kEyeReflect, kTreeAnim, kDecal, kDynamicDecal, kAnisotropicLighting);
-
-			auto ret = func(This, geometry, renderMode, accumulator);
-			This->flags = GetSingleton()->flagsBackup;
-			return ret;
-		}
-		static inline REL::Relocation<decltype(thunk)> func;
-	};
-
-	
-	struct EmplacePassesHook
-	{
-		static RE::BSRenderPass* thunk(RE::BSLightingShaderProperty::Data* This, RE::BSShader* shader, RE::BSLightingShaderProperty* property, RE::BSGeometry* geometry, uint32_t, uint8_t numLights, RE::BSLight* light0, RE::BSLight* light1, RE::BSLight* light2, RE::BSLight* light3)
-		{
-			using enum RE::BSLightingShaderProperty::EShaderPropertyFlag;
-			using enum Flags;
-
-			stl::enumeration<Flags> techniqueE;
-
-		//	const auto alphaProperty = static_cast<RE::NiAlphaProperty*>(geometry->GetGeometryRuntimeData().properties[0].get());
-
-			auto flags = GetSingleton()->flagsBackup;
-
-			stl::enumeration<Flags> technique;
-			//techniqueE.set(RenderDepth, Texture);
-			techniqueE.set(RenderShadowmap);
-
-			//if (flags.any(kModelSpaceNormals)) {
-			//	techniqueE.set(Normals, BinormalTangent);
-			//}
-			//if (flags.any(kVertexColors)) {
-			//	techniqueE.set(Vc);
-			//}
-			//if (alphaProperty && alphaProperty->GetAlphaTesting()) {
-			//	techniqueE.set(AlphaTest);
-			//}
-			//if (flags.any(kLODObjects, kHDLODObjects)) {
-			//	techniqueE.set(LodObject);
-			//}		
-			//if (flags.any(kTreeAnim)) {
-			//	techniqueE.set(TreeAnim);
-			//}
-			//if (flags.any(kSkinned)) {
-			//	techniqueE.set(Skinned);
-			//}
-
-			property->flags = GetSingleton()->flagsBackup;
-
-			auto pass = func(This, shader, property, geometry, techniqueE.underlying() + +static_cast<uint32_t>(0x2B), numLights, light0, light1, light2, light3);
-			if (flags.any(kTreeAnim)) {
-			//	pass->accumulationHint = 11;
+				auto pass = precipitationOcclusionMapRenderPassList->EmplacePass(
+					BSUtilityShader::GetSingleton(),
+					property,
+					geometry,
+					technique.underlying() + static_cast<uint32_t>(ShaderTechnique::UtilityGeneralStart));
+				if (property->flags.any(kTreeAnim))
+					pass->accumulationHint = 11;
 			}
-			return pass;
-		}
-		static inline REL::Relocation<decltype(thunk)> func;
-	};
-
-	struct CreateRenderTarget_Occlusion
-	{
-		static void thunk(RE::BSGraphics::Renderer* This, RE::RENDER_TARGETS_DEPTHSTENCIL a_target, RE::BSGraphics::DepthStencilTargetProperties* a_properties)
-		{
-			a_properties->height = 2048;
-			a_properties->width = 2048;
-
-			func(This, a_target, a_properties);
-		}
+			return precipitationOcclusionMapRenderPassList;
+		};
 		static inline REL::Relocation<decltype(thunk)> func;
 	};
 
@@ -259,9 +297,8 @@ public:
 	{
 		static void Install()
 		{
+			logger::info("Hooking BSLightingShaderProperty");
 			stl::write_vfunc<0x2D, BSLightingShaderProperty_GetPrecipitationOcclusionMapRenderPassesImpl>(RE::VTABLE_BSLightingShaderProperty[0]);
-			stl::write_thunk_call<EmplacePassesHook>(REL::RelocationID(99874, 36559).address() + REL::Relocate(0x1AD, 0x841, 0x791));
-			stl::write_thunk_call<CreateRenderTarget_Occlusion>(REL::RelocationID(100458, 107175).address() + REL::Relocate(0x1245, 0x3F3, 0x548));
 
 		}
 	};
