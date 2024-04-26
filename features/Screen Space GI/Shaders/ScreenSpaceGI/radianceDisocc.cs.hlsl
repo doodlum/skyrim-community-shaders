bd8383032ee5110c7e3447c68f20a95742014568
Pentalimbed <pentalimbed@gmail.com>
2024-04-26 23:54:12 +0100

    chore: ssgi improvements

--- features/Screen Space GI/Shaders/ScreenSpaceGI/radianceDisocc.cs.hlsl
+++ features/Screen Space GI/Shaders/ScreenSpaceGI/radianceDisocc.cs.hlsl
@@ -19,8 +19,39 @@ RWTexture2D<float4> outRemappedPrevGI : register(u2);
 #	define REPROJECTION
 #endif
 
-[numthreads(8, 8, 1)] void main(const uint2 pixCoord
-								: SV_DispatchThreadID) {
+void readHistory(
+	uint eyeIndex, float3 curr_pos, float3 curr_normal, int2 pixCoord, float bilinear_weight,
+	inout half4 prev_gi, inout half4 prev_gi_albedo, inout float accum_frames, inout float wsum)
+{
+	const float2 uv = (pixCoord + .5) * RcpOutFrameDim;
+	const float2 screen_pos = ConvertToStereoUV(uv, eyeIndex);
+	if (any(screen_pos < 0) || any(screen_pos > 1))
+		return;
+
+	const half3 prev_geo = srcPrevGeo[pixCoord];
+	const float prev_depth = prev_geo.x;
+	const float3 prev_normal = DecodeNormal(prev_geo.yz);  // prev normal is already world
+	float3 prev_pos = ScreenToViewPosition(screen_pos, prev_depth, eyeIndex);
+	prev_pos = ViewToWorldPosition(prev_pos, PrevInvViewMat[eyeIndex]);
+
+	float3 delta_pos = curr_pos - prev_pos;
+	float normal_prod = dot(curr_normal, prev_normal);
+
+	bool depth_pass = dot(delta_pos, delta_pos) < DepthDisocclusion * DepthDisocclusion;
+	bool normal_pass = normal_prod * normal_prod > NormalDisocclusion;
+	if (depth_pass && normal_pass) {
+#if defined(GI) && defined(GI_BOUNCE)
+		prev_gi_albedo += srcPrevGIAlbedo[pixCoord] * bilinear_weight;
+#endif
+#ifdef TEMPORAL_DENOISER
+		prev_gi += srcPrevGI[pixCoord] * bilinear_weight;
+		accum_frames += srcAccumFrames[pixCoord] * bilinear_weight;
+#endif
+		wsum += bilinear_weight;
+	}
+};
+
+[numthreads(8, 8, 1)] void main(const uint2 pixCoord : SV_DispatchThreadID) {
 	const float srcScale = SrcFrameDim * RcpTexDim;
 	const float outScale = OutFrameDim * RcpTexDim;
 
@@ -34,10 +65,12 @@ RWTexture2D<float4> outRemappedPrevGI : register(u2);
 #endif
 	float2 prev_screen_pos = ConvertToStereoUV(prev_uv, eyeIndex);
 
+	half4 prev_gi_albedo = 0;
+	half4 prev_gi = 0;
+	float accum_frames = 0;
+	float wsum = 0;
+
 	const float curr_depth = READ_DEPTH(srcCurrDepth, pixCoord);
-
-	bool valid_history = false;
-
 #ifdef REPROJECTION
 	if ((curr_depth <= DepthFadeRange.y) && !(any(prev_screen_pos < 0) || any(prev_screen_pos > 1))) {
 		float3 curr_normal = DecodeNormal(FULLRES_LOAD(srcCurrNormal, pixCoord, uv * srcScale, samplerLinearClamp).xy);
@@ -45,33 +78,44 @@ RWTexture2D<float4> outRemappedPrevGI : register(u2);
 		float3 curr_pos = ScreenToViewPosition(screen_pos, curr_depth, eyeIndex);
 		curr_pos = ViewToWorldPosition(curr_pos, InvViewMatrix[eyeIndex]);
 
-		const half3 prev_geo = srcPrevGeo.SampleLevel(samplerPointClamp, prev_uv * outScale, 0);
-		const float prev_depth = prev_geo.x;
-		const float3 prev_normal = DecodeNormal(prev_geo.yz);  // prev normal is already world
-		float3 prev_pos = ScreenToViewPosition(prev_screen_pos, prev_depth, eyeIndex);
-		prev_pos = ViewToWorldPosition(prev_pos, PrevInvViewMat[eyeIndex]);
+		float2 prev_px_coord = prev_uv * OutFrameDim;
+		int2 prev_px_lu = floor(prev_px_coord - 0.5);
+		float2 bilinear_weights = prev_px_coord - 0.5 - prev_px_lu;
+		{
+			int2 px = prev_px_lu;
+			float w = (1 - bilinear_weights.x) * (1 - bilinear_weights.y);
+			readHistory(eyeIndex, curr_pos, curr_normal, px, w,
+				prev_gi, prev_gi_albedo, accum_frames, wsum);
+		}
+		{
+			int2 px = prev_px_lu + uint2(1, 0);
+			float w = bilinear_weights.x * (1 - bilinear_weights.y);
+			readHistory(eyeIndex, curr_pos, curr_normal, px, w,
+				prev_gi, prev_gi_albedo, accum_frames, wsum);
+		}
+		{
+			int2 px = prev_px_lu + uint2(0, 1);
+			float w = (1 - bilinear_weights.x) * bilinear_weights.y;
+			readHistory(eyeIndex, curr_pos, curr_normal, px, w,
+				prev_gi, prev_gi_albedo, accum_frames, wsum);
+		}
+		{
+			int2 px = prev_px_lu + uint2(1, 1);
+			float w = bilinear_weights.x * bilinear_weights.y;
+			readHistory(eyeIndex, curr_pos, curr_normal, px, w,
+				prev_gi, prev_gi_albedo, accum_frames, wsum);
+		}
 
-		float3 delta_pos = curr_pos - prev_pos;
-		float normal_prod = dot(curr_normal, prev_normal);
-
-		bool depth_pass = dot(delta_pos, delta_pos) < DepthDisocclusion * DepthDisocclusion;
-		bool normal_pass = normal_prod * normal_prod > NormalDisocclusion;
-		valid_history = depth_pass && normal_pass;
-	}
-#endif
-
-	half4 prev_gi_albedo = 0;
-	half4 prev_gi = 0;
-
-#ifdef REPROJECTION
-	[branch] if (valid_history)
-	{
-#	if defined(GI) && defined(GI_BOUNCE)
-		prev_gi_albedo = srcPrevGIAlbedo.SampleLevel(samplerLinearClamp, prev_uv * outScale, 0);
-#	endif
+		if (wsum > 1e-2) {
+			float rcpWsum = rcp(wsum + 1e-2);
 #	ifdef TEMPORAL_DENOISER
-		prev_gi = srcPrevGI.SampleLevel(samplerLinearClamp, prev_uv * outScale, 0);
+			prev_gi *= rcpWsum;
+			accum_frames *= rcpWsum;
 #	endif
+#	if defined(GI) && defined(GI_BOUNCE)
+			prev_gi_albedo *= rcpWsum;
+#	endif
+		}
 	}
 #endif
 
@@ -85,11 +129,7 @@ RWTexture2D<float4> outRemappedPrevGI : register(u2);
 #endif
 
 #ifdef TEMPORAL_DENOISER
-	uint accum_frames = 0;
-	[branch] if (valid_history)
-		accum_frames = srcAccumFrames.SampleLevel(samplerLinearClamp, prev_uv * outScale, 0) * 255;
-	accum_frames = min(accum_frames + 1, MaxAccumFrames);
-
+	accum_frames = min(accum_frames * 255 + 1, MaxAccumFrames);
 	outAccumFrames[pixCoord] = accum_frames / 255.0;
 	outRemappedPrevGI[pixCoord] = prev_gi;
 #endif
