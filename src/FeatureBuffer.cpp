3ab43446f6609ec71fdce338ae5ca538c3550a8a
Pentalimbed <pentalimbed@gmail.com>
2024-06-09 19:36:55 +0100

    chore: hide the templates

--- src/FeatureBuffer.cpp
+++ src/FeatureBuffer.cpp
@@ -4,6 +4,22 @@
 #include "Features/ExtendedMaterials.h"
 #include "Features/GrassLighting.h"
 
+template <class... Ts>
+std::pair<unsigned char*, size_t> _GetFeatureBufferData(Ts... feat_datas)
+{
+	size_t totalSize = (... + sizeof(Ts));
+	auto data = new unsigned char[totalSize];
+	size_t offset = 0;
+
+	([&] {
+		*((decltype(feat_datas)*)(data + offset)) = feat_datas;
+		offset += sizeof(decltype(feat_datas));
+	}(),
+		...);
+
+	return std::make_pair(data, totalSize);
+}
+
 std::pair<unsigned char*, size_t> GetFeatureBufferData()
 {
 	return _GetFeatureBufferData(
