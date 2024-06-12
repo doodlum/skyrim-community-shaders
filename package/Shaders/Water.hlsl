41781685afb3ee0a382848f4597367f495a71172
Tim <15017472+doodlum@users.noreply.github.com>
2024-06-12 16:57:33 +0100

    chore: minimum ambient water amount

--- package/Shaders/Water.hlsl
+++ package/Shaders/Water.hlsl
@@ -607,11 +607,12 @@ float3 GetWaterDiffuseColor(PS_INPUT input, float3 normal, float3 viewDirection,
 	float3 refractionDiffuseColor = lerp(ShallowColor.xyz, DeepColor.xyz, distanceMul.y);
 
 	if (!(PixelShaderDescriptor & _Interior)) {
+	{
 		float vl = GetVL(input.WPosition.xyz, refractionWorldPosition.xyz, screenPosition) * (dot(viewDirection, SunDir.xyz) * 0.5 + 0.5);
 
 		float3 refractionDiffuseColorSunlight = refractionDiffuseColor * vl * SunColor.xyz * SunDir.w;	
 #	if defined(SKYLIGHTING)		
-		float3 refractionDiffuseColorSkylight = refractionDiffuseColor * GetVLSkylighting(input.WPosition.xyz, refractionWorldPosition.xyz, screenPosition);
+		float3 refractionDiffuseColorSkylight = refractionDiffuseColor * lerp(GetVLSkylighting(input.WPosition.xyz, refractionWorldPosition.xyz, screenPosition), 1.0, 0.25);
 		refractionDiffuseColor = refractionDiffuseColorSkylight;
 #	endif
 		refractionDiffuseColor += refractionDiffuseColorSunlight;
