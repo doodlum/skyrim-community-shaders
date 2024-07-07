834e8d614ceeee47af4a3c57140529fae75f11f6
Pentalimbed <pentalimbed@gmail.com>
2024-07-07 17:33:58 +0100

    chore: convolve and ZH3 hallucination for diffuse skylighitng

--- package/Shaders/AmbientCompositeCS.hlsl
+++ package/Shaders/AmbientCompositeCS.hlsl
@@ -1,14 +1,14 @@
+#include "Common/Color.hlsl"
 #include "Common/DeferredShared.hlsli"
 #include "Common/FrameBuffer.hlsl"
 #include "Common/GBuffer.hlsli"
 #include "Common/VR.hlsli"
-#include "Common/Color.hlsl"
 
 Texture2D<unorm half3> AlbedoTexture : register(t0);
 Texture2D<unorm half3> NormalRoughnessTexture : register(t1);
 
 #if defined(SKYLIGHTING)
-#include "Common/Spherical Harmonics/SphericalHarmonics.hlsli"
+#	include "Common/Spherical Harmonics/SphericalHarmonics.hlsli"
 Texture2D<unorm float4> SkylightingTexture : register(t2);
 #endif
 
@@ -39,11 +39,22 @@ RWTexture2D<half3> DiffuseAmbientRW : register(u1);
 	half3 ambient = albedo * directionalAmbientColor;
 
 	diffuseColor = sRGB2Lin(diffuseColor);
-	ambient = sRGB2Lin(max(0, ambient)); // Fixes black blobs on the world map
+	ambient = sRGB2Lin(max(0, ambient));  // Fixes black blobs on the world map
 	albedo = sRGB2Lin(albedo);
 
 #if defined(SKYLIGHTING)
-	half skylighting = lerp(0.1, 1.0, saturate(shUnproject(SkylightingTexture[dispatchID.xy], normalWS)));
+	// ZH3 hallucination, url: http://torust.me/ZH3.pdf
+	sh2 skylightingSH = SkylightingTexture[dispatchID.xy];
+	half3 zonalAxis = normalize(half3(-skylightingSH.w, -skylightingSH.y, skylightingSH.z) + 1e-10);
+	half ratio = abs(dot(half3(-skylightingSH.w, -skylightingSH.y, skylightingSH.z), zonalAxis)) / (skylightingSH.x + 1e-10);
+	half zonalL2Coeff = skylightingSH.x * (0.08f * ratio + 0.6f * ratio * ratio);
+	half fZ = dot(zonalAxis, normalWS);
+	half zhDir = sqrt(5.0f / (16.0f * shPI)) * (3.0f * fZ * fZ - 1.0f);
+
+	half skylighting = lerp(0.2, 1.0, saturate(dot(skylightingSH, shEvaluateCosineLobe(normalWS))));
+
+	skylighting += 0.25f * zonalL2Coeff * zhDir;
+
 	ambient *= skylighting;
 #endif
 
