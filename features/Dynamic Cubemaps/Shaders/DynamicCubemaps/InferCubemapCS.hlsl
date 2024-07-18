4f7fd448ed12447bc324028ae42d1f67df2bade4
Tim <15017472+doodlum@users.noreply.github.com>
2024-07-18 19:56:46 +0100

    refactor: divide instead of multiply

--- features/Dynamic Cubemaps/Shaders/DynamicCubemaps/InferCubemapCS.hlsl
+++ features/Dynamic Cubemaps/Shaders/DynamicCubemaps/InferCubemapCS.hlsl
@@ -90,9 +90,9 @@ float3 GetSamplingVector(uint3 ThreadID, in RWTexture2DArray<float4> OutputTextu
 	}
 
 #if defined(REFLECTIONS)
-	color.rgb = lerp(color.rgb, sRGB2Lin(ReflectionsTexture.SampleLevel(LinearSampler, uv, 0)), saturate(mipLevel * (1.0 / 8.0)));
+	color.rgb = lerp(color.rgb, sRGB2Lin(ReflectionsTexture.SampleLevel(LinearSampler, uv, 0)), saturate(mipLevel / 8.0));
 #else
-	color.rgb = lerp(color.rgb, color.rgb * sRGB2Lin(DefaultCubemap.SampleLevel(LinearSampler, uv, 0).x) * 2, saturate(mipLevel * (1.0 / 8.0)));
+	color.rgb = lerp(color.rgb, color.rgb * sRGB2Lin(DefaultCubemap.SampleLevel(LinearSampler, uv, 0).x) * 2, saturate(mipLevel / 8.0));
 #endif
 
 	color.rgb = Lin2sRGB(color.rgb);
