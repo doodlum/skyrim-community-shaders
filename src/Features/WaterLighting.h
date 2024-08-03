e69d1afec50199f1515e350d099b8b05342db606
Flayan <hannes.feldt@gmail.com>
2024-08-03 18:47:07 +0200

    fix: add back water lighting cpp files (#379)

---
+++ src/Features/WaterLighting.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Feature.h"
+
+struct WaterLighting : Feature
+{
+public:
+	static WaterLighting* GetSingleton()
+	{
+		static WaterLighting singleton;
+		return &singleton;
+	}
+
+	ID3D11ShaderResourceView* causticsView;
+
+	virtual inline std::string GetName() override { return "Water Lighting"; }
+	virtual inline std::string GetShortName() override { return "WaterLighting"; }
+	virtual inline std::string_view GetShaderDefineName() override { return "WATER_LIGHTING"; }
+
+	bool HasShaderDefine(RE::BSShader::Type shaderType) override;
+
+	virtual void SetupResources() override;
+
+	virtual void Prepass() override;
+
+	virtual bool SupportsVR() override { return true; };
+};
