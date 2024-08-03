99702601ee0d32db8bb0d0485f257d0c81605aea
Jonahex <Jonahex@users.noreply.github.com>
2024-08-03 20:29:36 +0000

    style: 🎨 apply clang-format changes

--- package/Shaders/ISSILComposite.hlsl
+++ package/Shaders/ISSILComposite.hlsl
@@ -8,18 +8,17 @@ struct PS_OUTPUT
 	float4 Color : SV_Target0;
 };
 
-#	if defined(PSHADER)
+#if defined(PSHADER)
 SamplerState sourceSampler : register(s0);
 SamplerState IndLightingSampler : register(s1);
 
 Texture2D<float4> sourceTex : register(t0);
 Texture2D<float4> IndLightingTex : register(t1);
 
-
 PS_OUTPUT main(PS_INPUT input)
 {
 	PS_OUTPUT psout;
-	
+
 	float4 sourceColor = sourceTex.Sample(sourceSampler, input.TexCoord);
 	float4 ilColor = IndLightingTex.Sample(IndLightingSampler, input.TexCoord);
 
@@ -27,4 +26,4 @@ PS_OUTPUT main(PS_INPUT input)
 
 	return psout;
 }
-#	endif
+#endif
