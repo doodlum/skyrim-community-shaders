85b4394d2bbf067c0dffa6ef70feb3759c79a446
Tim <15017472+doodlum@users.noreply.github.com>
2024-08-22 20:48:44 +0100

    fix: AmbientComposite in linear for PBR

--- package/Shaders/AmbientCompositeCS.hlsl
+++ package/Shaders/AmbientCompositeCS.hlsl
@@ -55,6 +55,8 @@ RWTexture2D<half3> DiffuseAmbientRW : register(u1);
 	half3 linDirectionalAmbientColor = sRGB2Lin(directionalAmbientColor);
 	half3 linDiffuseColor = sRGB2Lin(diffuseColor);
 
+	half3 linAmbient = lerp(sRGB2Lin(albedo * directionalAmbientColor), linAlbedo * linDirectionalAmbientColor, pbrWeight);
+
 	half visibility = 1.0;
 #if defined(SKYLIGHTING)
 	float rawDepth = DepthTexture[dispatchID.xy];
@@ -87,10 +89,11 @@ RWTexture2D<half3> DiffuseAmbientRW : register(u1);
 	linDiffuseColor += ssgiDiffuse.rgb;
 #endif
 
+	linAmbient *= visibility;
 	diffuseColor = Lin2sRGB(linDiffuseColor);
 	directionalAmbientColor = Lin2sRGB(linDirectionalAmbientColor * visibility);
 
-	diffuseColor = diffuseColor + directionalAmbientColor * albedo;
+	diffuseColor = lerp(diffuseColor + directionalAmbientColor * albedo, Lin2sRGB(linDiffuseColor + linAmbient), pbrWeight);
 
 	MainRW[dispatchID.xy] = diffuseColor;
 };
