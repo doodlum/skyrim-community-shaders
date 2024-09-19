doodlum/skyrim-community-shaders

commit c64421048a664ffdcc33d7b3666e8ef36ef6c440

feat: llf cleanup and optimisations, plus shadow improvements (#419)
* feat: llf cleanup and optimisations

* style: 🎨 apply clang-format changes

* fix: added missing water shader changes

* fix: grass compile error

* feat: more shadow optimisations

* feat: shadows on transparent lighting shaders

* fix: shadows too sharp

* style: 🎨 apply clang-format changes

* fix: too aggressive shadow bias adjustment

* performance: store contact shadow steps function

* chore: removed contact shadows from grass

* style: 🎨 apply clang-format changes

* revert: back to vanilla shadow blurriness

* feat: improved shadow sampling

* style: 🎨 apply clang-format changes

* feat: blurred contact shadows

* style: 🎨 apply clang-format changes

* fix: clamp shadow sampling uvs, stochastic sampling

* style: 🎨 apply clang-format changes

* performance: shared 3D noise outside of contact shadows

* style: 🎨 apply clang-format changes

* chore: cleanup water vl sampling code

---------

Co-authored-by: doodlum <doodlum@users.noreply.github.com>

features/Light Limit Fix/Shaders/LightLimitFix/LightLimitFix.hlsli
@@ -11,17 +11,15 @@ struct StructuredLight
 	float radius;
 	float4 positionWS[2];
 	float4 positionVS[2];
-	uint firstPersonShadow;
-	float pad0[3];
 };
 
 struct StrictLightData
 {
 	StructuredLight StrictLights[15];
 	uint NumStrictLights;
-	bool EnableGlobalLights;
 	float LightsNear;
 	float LightsFar;
+	uint pad0;
 };
 
 StructuredBuffer<StructuredLight> lights : register(t50);
@@ -47,30 +45,29 @@ bool GetClusterIndex(in float2 uv, in float z, out uint clusterIndex)
 bool IsSaturated(float value) { return value == saturate(value); }
 bool IsSaturated(float2 value) { return IsSaturated(value.x) && IsSaturated(value.y); }
 
-float ContactShadows(float3 rayPos, float2 texcoord, float offset, float3 lightDirectionVS, float radius, bool longShadow, uint a_eyeIndex = 0)
+float ContactShadows(float3 viewPosition, float noise2D, float3 noise3D, float3 lightDirectionVS, uint contactShadowSteps, uint a_eyeIndex = 0)
 {
-	float2 depthDeltaMult;
-	uint loopMax;
-	if (longShadow) {
-		loopMax = log2(radius);
-		depthDeltaMult = float2(1.0 * loopMax, 1.0 / loopMax) * 0.10;
-		lightDirectionVS *= loopMax * 4.0;
-	} else {
-		loopMax = log2(radius) * 0.5;
-		depthDeltaMult = float2(1.0 * loopMax, 1.0 / loopMax) * 0.20;
-		lightDirectionVS *= loopMax;
-	}
+	if (contactShadowSteps == 0)
+		return 1.0;
+
+	float2 depthDeltaMult = float2(1.0, 0.05);
+
+	// Extend contact shadow distance
+	lightDirectionVS *= 2.0;
 
 	// Offset starting position with interleaved gradient noise
-	rayPos += lightDirectionVS * offset;
+	viewPosition += lightDirectionVS * noise2D;
+
+	// Offset starting position to simulate gaussian blur
+	viewPosition += noise3D;
 
 	// Accumulate samples
-	float shadow = 0.0;
-	[loop] for (uint i = 0; i < loopMax; i++)
-	{
+	float contactShadow = 0.0;
+	for (uint i = 0; i < contactShadowSteps; i++) {
 		// Step the ray
-		rayPos += lightDirectionVS;
-		float2 rayUV = ViewToUV(rayPos, true, a_eyeIndex);
+		viewPosition += lightDirectionVS;
+
+		float2 rayUV = ViewToUV(viewPosition, true, a_eyeIndex);
 
 		// Ensure the UV coordinates are inside the screen
 		if (!IsSaturated(rayUV))
@@ -80,12 +77,12 @@ float ContactShadows(float3 rayPos, float2 texcoord, float offset, float3 lightD
 		float rayDepth = GetScreenDepth(rayUV, a_eyeIndex);
 
 		// Difference between the current ray distance and the marched light
-		float depthDelta = rayPos.z - rayDepth;
+		float depthDelta = viewPosition.z - rayDepth;
 		if (rayDepth > 16.5)  // First person
-			shadow += saturate(depthDelta * depthDeltaMult.x) - saturate(depthDelta * depthDeltaMult.y);
+			contactShadow += saturate(depthDelta * depthDeltaMult.x) - saturate(depthDelta * depthDeltaMult.y);
 	}
 
-	return 1.0 - saturate(shadow);
+	return 1.0 - saturate(contactShadow);
 }
 
 // Copyright 2019 Google LLC.

src/Features/LightLimitFix.h
@@ -37,8 +37,6 @@ public:
 		float radius;
 		PositionOpt positionWS[2];
 		PositionOpt positionVS[2];
-		uint firstPersonShadow;
-		float pad0[3];
 	};
 
 	struct ClusterAABB
@@ -84,9 +82,9 @@ public:
 	{
 		LightData StrictLights[15];
 		uint NumStrictLights;
-		uint EnableGlobalLights;
 		float LightsNear;
 		float LightsFar;
+		uint pad0;
 	};
 
 	StrictLightData strictLightDataTemp;
@@ -155,7 +153,6 @@ public:
 	struct Settings
 	{
 		bool EnableContactShadows = false;
-		bool EnableFirstPersonShadows = false;
 		bool EnableLightsVisualisation = false;
 		uint LightsVisualisationMode = 0;
 		bool EnableParticleLights = true;
@@ -341,12 +338,11 @@ struct fmt::formatter<LightLimitFix::LightData>
 	auto format(const LightLimitFix::LightData& l, format_context& ctx) const -> format_context::iterator
 	{
 		// ctx.out() is an output iterator to write to.
-		return fmt::format_to(ctx.out(), "{{address {:x} color {} radius {} posWS {} {} posVS {} {} first-person shadow {}}}",
+		return fmt::format_to(ctx.out(), "{{address {:x} color {} radius {} posWS {} {} posVS {} {}}}",
 			reinterpret_cast<uintptr_t>(&l),
 			(Vector3)l.color,
 			l.radius,
 			(Vector3)l.positionWS[0].data, (Vector3)l.positionWS[1].data,
-			(Vector3)l.positionVS[0].data, (Vector3)l.positionVS[1].data,
-			l.firstPersonShadow);
+			(Vector3)l.positionVS[0].data, (Vector3)l.positionVS[1].data);
 	}
 };

features/Light Limit Fix/ParticleLights/mx_lightglow01.ini
@@ -1,2 +1,0 @@
-[Light]
-Cull = true

features/Light Limit Fix/ParticleLights/mx_lightglow02.ini
@@ -1,2 +1,0 @@
-[Light]
-Cull = true

features/Light Limit Fix/ParticleLights/mx_lightglow03.ini
@@ -1,2 +1,0 @@
-[Light]
-Cull = true

features/Light Limit Fix/ParticleLights/mx_lightglow04.ini
@@ -1,2 +1,0 @@
-[Light]
-Cull = true

features/Light Limit Fix/ParticleLights/mx_lightglow05.ini
@@ -1,2 +1,0 @@
-[Light]
-Cull = true 

package/Shaders/Common/Random.hlsli
@@ -1,6 +1,18 @@
 #ifndef CS_RANDOM
 #define CS_RANDOM
 
+// https://www.iryoku.com/next-generation-post-processing-in-call-of-duty-advanced-warfare/
+const static float2 SpiralSampleOffsets8[] = {
+	{ -0.7071, 0.7071 },
+	{ -0.0000, -0.8750 },
+	{ 0.5303, 0.5303 },
+	{ -0.6250, -0.0000 },
+	{ 0.3536, -0.3536 },
+	{ -0.0000, 0.3750 },
+	{ -0.1768, -0.1768 },
+	{ 0.1250, 0.0000 }
+};
+
 ///////////////////////////////////////////////////////////
 // WHITE-LIKE HASHES
 ///////////////////////////////////////////////////////////

commit dbb0df9ff13e95fcb52eb48091ec616c4315e0e6

fix: fix broken vr lights due to buffer alignment
Fixes issue introduced by c64421048a664ffdcc33d7b3666e8ef36ef6c440

features/Light Limit Fix/Shaders/LightLimitFix/LightLimitFix.hlsli
@@ -11,6 +11,7 @@ struct StructuredLight
 	float radius;
 	float4 positionWS[2];
 	float4 positionVS[2];
+	float pad0[4];
 };
 
 struct StrictLightData

src/Features/LightLimitFix.h
@@ -37,6 +37,7 @@ public:
 		float radius;
 		PositionOpt positionWS[2];
 		PositionOpt positionVS[2];
+		float pad0[4];
 	};
 
 	struct ClusterAABB