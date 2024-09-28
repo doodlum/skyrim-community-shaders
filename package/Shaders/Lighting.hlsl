af756d8d4dd1263ef0d215d36489a47105487d4c
Jonahex <Jonahex@users.noreply.github.com>
2024-09-28 15:07:10 +0000

    style: 🎨 apply clang-format changes

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2131,8 +2131,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace
 		float lightShadow = 1.f;
 
 		float shadowComponent = 1.0;
-        if ((PixelShaderDescriptor & _DefShadow) && (light.lightFlags & Llf_ShadowLight))
-        {
+		if ((PixelShaderDescriptor & _DefShadow) && (light.lightFlags & Llf_ShadowLight)) {
 			shadowComponent = shadowColor[light.shadowLightIndex];
 			lightShadow *= shadowComponent;
 		}
