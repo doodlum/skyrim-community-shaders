d974cfa25bcbbb377ecdc5189def77e64cd98146
doodlum <15017472+doodlum@users.noreply.github.com>
2024-09-29 15:47:54 +0100

    fix: llf fixed alignment issues (#573)
    * fix: llf fixed alignment issues
    
    * style: 🎨 apply clang-format changes
    
    ---------
    
    Co-authored-by: doodlum <doodlum@users.noreply.github.com>

--- features/Light Limit Fix/Shaders/LightLimitFix/ClusterBuildingCS.hlsl
+++ features/Light Limit Fix/Shaders/LightLimitFix/ClusterBuildingCS.hlsl
@@ -61,8 +61,8 @@ float3 IntersectionZPlane(float3 B, float z_dist)
 	float3 minPointVS = min(GetPositionVS(texcoordMin, 1.0f, 0), GetPositionVS(texcoordMin, 1.0f, 1));
 #endif  // !VR
 
-	float clusterNear = LightsNear * pow(LightsFar / max(LightsNear, 1e-5), groupId.z / float(CLUSTER_BUILDING_DISPATCH_SIZE_Z));
-	float clusterFar = LightsNear * pow(LightsFar / max(LightsNear, 1e-5), (groupId.z + 1) / float(CLUSTER_BUILDING_DISPATCH_SIZE_Z));
+	float clusterNear = LightsNear * pow(LightsFar / LightsNear, groupId.z / float(CLUSTER_BUILDING_DISPATCH_SIZE_Z));
+	float clusterFar = LightsNear * pow(LightsFar / LightsNear, (groupId.z + 1) / float(CLUSTER_BUILDING_DISPATCH_SIZE_Z));
 
 	float3 minPointNear = IntersectionZPlane(minPointVS, clusterNear);
 	float3 minPointFar = IntersectionZPlane(minPointVS, clusterFar);
