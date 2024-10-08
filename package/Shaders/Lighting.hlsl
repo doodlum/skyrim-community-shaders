41112805f7a95a3d0bc2c9db8ec6208ca5cb8bdd
Alan Tse <alandtse@gmail.com>
2024-10-07 22:37:09 -0700

    fix(VR): fix VR desync
    closes #601

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2132,7 +2132,13 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace
 
 		float shadowComponent = 1.0;
 		if ((PixelShaderDescriptor & _DefShadow) && (light.lightFlags & Llf_ShadowLight)) {
-			shadowComponent = shadowColor[light.shadowLightIndex];
+			shadowComponent = shadowColor[
+#			ifndef VR
+				light.shadowLightIndex
+#			else
+				ShadowLightMaskSelect[light.shadowLightIndex]
+#			endif
+			];
 			lightShadow *= shadowComponent;
 		}
 
