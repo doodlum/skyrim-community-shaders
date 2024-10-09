1d677820d366e5fe11348138efd63aaec1ea2ae0
Alan Tse <alandtse@users.noreply.github.com>
2024-10-09 04:07:07 -0700

    refactor: move hooks out of State (#599)
    * refactor: move hooks out of State
    
    Hooks should be in XSEPlugin so they can all be found. A similar
    refactor should be done for SetupResources and other areas where
    State is being piggybacked.
    
    * refactor: move d3dhooks to query

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -1,10 +1,12 @@
 #include "Hooks.h"
 
+#include "Deferred.h"
 #include "FrameAnnotations.h"
 #include "Menu.h"
 #include "ShaderCache.h"
 #include "State.h"
 #include "TruePBR.h"
+#include "Upscaling.h"
 
 #include "ENB/ENBSeriesAPI.h"
 #include "Features/ExtendedMaterials.h"
@@ -78,7 +80,11 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 	case SKSE::MessagingInterface::kPostPostLoad:
 		{
 			if (errors.empty()) {
-				State::GetSingleton()->PostPostLoad();
+				auto state = State::GetSingleton();
+				state->PostPostLoad();  // state should load first so basic information is populated
+				Deferred::Hooks::Install();
+				TruePBR::GetSingleton()->PostPostLoad();
+				Upscaling::InstallHooks();
 				Hooks::Install();
 				FrameAnnotations::OnPostPostLoad();
 
@@ -165,6 +171,5 @@ bool Load()
 
 	if (errors.empty())
 		Hooks::InstallD3DHooks();
-
 	return true;
 }
