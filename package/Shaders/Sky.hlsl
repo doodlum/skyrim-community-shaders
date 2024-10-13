a6a1177bd83c25441e2e42e67c1f10f6aced6f72
Tim <15017472+doodlum@users.noreply.github.com>
2024-10-13 15:37:20 +0100

    fix: sky motion vectors

--- package/Shaders/Sky.hlsl
+++ package/Shaders/Sky.hlsl
@@ -241,7 +241,15 @@ PS_OUTPUT main(PS_INPUT input)
 	psout.Color = float4(0, 0, 0, 1.0);
 #	endif  // OCCLUSION
 
-	float2 screenMotionVector = GetSSMotionVector(input.WorldPosition, input.PreviousWorldPosition, eyeIndex);
+	float3 viewPosition = mul(CameraView[eyeIndex], float4(input.WorldPosition.xyz, 1)).xyz;
+	float2 screenUV = FrameBuffer::ViewToUV(viewPosition, true, eyeIndex);
+
+	float4 positionWS = half4(2 * half2(screenUV.x, -screenUV.y + 1) - 1, 1.0, 1);
+	positionWS = mul(CameraViewProjInverse[eyeIndex], positionWS);
+	positionWS.xyz = positionWS.xyz / positionWS.w;
+	positionWS.w = 1;
+	
+	float2 screenMotionVector = GetSSMotionVector(positionWS, positionWS, eyeIndex);
 
 	psout.MotionVectors = float4(screenMotionVector, 0, psout.Color.w);
 	psout.Normal = float4(0.5, 0.5, 0, psout.Color.w);
