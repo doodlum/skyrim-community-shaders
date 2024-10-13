69875839443b6ff111115d7eefb4a487d70bbf8c
Tim <15017472+doodlum@users.noreply.github.com>
2024-10-13 21:40:18 +0100

    fix: set UI to 200 nits

--- package/Shaders/HDROutputCS.hlsl
+++ package/Shaders/HDROutputCS.hlsl
@@ -10,17 +10,17 @@ RWTexture2D<float4> HDRTexture : register(u0);
 [numthreads(8, 8, 1)] void main(uint3 dispatchID
 								: SV_DispatchThreadID) {
 	float3 backbuffer = BackBuffer[dispatchID.xy];
-	backbuffer = PQToLinear(backbuffer, 1.0) * (10000.0 / 203.0);
+	backbuffer = PQToLinear(backbuffer, 1.0) * (10000.0 / 100.0);
 	backbuffer = BT2020ToBT709(backbuffer);
 	backbuffer = sign(backbuffer) * Color::LinearToGamma(abs(backbuffer));
 
 	float4 ui = UI[dispatchID.xy];
 
-	float3 finalLinearColor = ui.xyz + backbuffer * (1.0 - ui.w);
+	float3 finalLinearColor = (ui.xyz * 2.0) + backbuffer * (1.0 - ui.w);
 
 	finalLinearColor = sign(finalLinearColor) * Color::GammaToLinear(abs(finalLinearColor));
 	finalLinearColor = BT709ToBT2020(finalLinearColor);
-	finalLinearColor = LinearToPQ(finalLinearColor / (10000.0 / 203.0), 1.0);
+	finalLinearColor = LinearToPQ(finalLinearColor / (10000.0 / 100.0), 1.0);
 
 	HDRTexture[dispatchID.xy] = float4(finalLinearColor, 1.0);
 };
