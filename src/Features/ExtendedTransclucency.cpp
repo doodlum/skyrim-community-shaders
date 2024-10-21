46913eb455d8a90fa23a16cf3cd767a1abbb8dc2
Arc Earth <ArcEarth@outlook.com>
2024-10-21 00:34:52 -0700

    Improve Settings UI doc

--- src/Features/ExtendedTransclucency.cpp
+++ src/Features/ExtendedTransclucency.cpp
@@ -10,7 +10,7 @@ NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(
 
 void ExtendedTransclucency::DrawSettings()
 {
-	if (ImGui::TreeNodeEx("Complex Transcluency", ImGuiTreeNodeFlags_DefaultOpen)) {
+	if (ImGui::TreeNodeEx("View Dependent Transcluency", ImGuiTreeNodeFlags_DefaultOpen)) {
 		static const char* AlphaModeNames[4] = {
 			"Anisotropic Fabric",
 			"Isotropic Fabric",
@@ -19,15 +19,19 @@ void ExtendedTransclucency::DrawSettings()
 		};
 		ImGui::Combo("Material Model", (int*)&settings.ViewDependentAlphaMode, AlphaModeNames, 4);
 		if (auto _tt = Util::HoverTooltipWrapper()) {
-			ImGui::Text("View dependent transparency will make a transcluent surface more opaque when you view it parallel to the surface.");
+			ImGui::Text("View dependent transparency will make a transcluent surface more opaque when you view it parallel to the surface.\n"
+			"  - Anisotropic Fabric: Common fabric weaved from tangent and birnormal direction, ignores normal map.\n"
+			"  - Isotropic Fabric: Imaginary fabric weaved from threads in one direction, respect normal map.\n"
+			"  - Rim Light: Naive rim light effect\n"
+			"  - Disabled: No view dependent transparency");
 		}
 
-		ImGui::SliderFloat("Transparency Increase Factor", &settings.AlphaReduction, 0.f, 1.f);
+		ImGui::SliderFloat("Increase Transparency", &settings.AlphaReduction, 0.f, 1.f);
 		if (auto _tt = Util::HoverTooltipWrapper()) {
 			ImGui::Text("VDT will make the material more opaque on average, which could be different from the intent, reduce the alpha to counter this effect and increase the dynamic range of the output.");
 		}
 
-		ImGui::SliderFloat("View Dependent Transparency Softness", &settings.AlphaSoftness, 0.0f, 1.0f);
+		ImGui::SliderFloat("Softness", &settings.AlphaSoftness, 0.0f, 1.0f);
 		if (auto _tt = Util::HoverTooltipWrapper()) {
 			ImGui::Text("Control the softness of VDT's alpha increase, reducing the increased amount of alpha.");
 		}
